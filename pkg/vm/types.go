3859b49018c9f38a04eba6177512e4f371f9247e
Jeff McCoy <code@jeffm.us>
2023-03-12 16:57:21 -0500

    v8 -> vm

--- pkg/vm/types.go
+++ pkg/vm/types.go
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-// v8 package contains the v8go wrapper for running JavaScript code in Zarf.
-package v8
+// vm package contains the v8go wrapper for running JavaScript code in Pepr.
+package vm
 
 import "rogchap.com/v8go"
 
