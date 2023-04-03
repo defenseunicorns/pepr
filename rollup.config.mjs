097f481b873848bd8c3bfb4edb61de24f15c4f8f
Jeff McCoy <code@jeffm.us>
2023-04-03 03:05:48 -0500

    starting to build `npx pepr`

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,36 +1,24 @@
 // rollup.config.js
 import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default {
-    external: [
-        /@kubernetes\/client-node(\/.*)?/,
-        'commander',
-        'express',
-        'fast-json-patch',
-        'ramda',
-    ],
     input: {
         cli: 'src/cli/index.ts',
         controller: 'src/controller/index.ts',
-        test: 'src/fixtures/index.ts',
     },
     output: {
         dir: 'dist',
         format: 'cjs',
         sourcemap: true,
-        entryFileNames: 'pepr-[name].js'
+        entryFileNames: 'pepr-[name].js',
+        banner: '#!/usr/bin/env node',
     },
     plugins: [
-        resolve({
-            preferBuiltins: true,
-        }),
         json(),
         typescript({
             tsconfig: "./tsconfig.json",
-            declaration: false,
             sourceMap: true,
         }),
         visualizer(),
