c64e37a3efa76081542f7837960a5a8b47f17ed3
Jeff McCoy <code@jeffm.us>
2023-04-15 01:19:09 -0500

    update e2e test

--- hack/e2e.test.js
+++ hack/e2e.test.js
@@ -1,19 +1,18 @@
 /* eslint-disable */
 const test = require("ava");
-const fs = require('fs').promises;
-const path = require('path');
-
+const fs = require("fs").promises;
+const path = require("path");
 const { spawn, execSync } = require("child_process");
 
-const TIMEOUT = 30 * 000; // Timeout in milliseconds
-
+// Timeout in milliseconds
+const TIMEOUT = 30 * 1000;
 const testDir = "pepr-test-module";
 
 let expectedLines = [
   "Establishing connection to Kubernetes",
   "Capability hello-pepr registered",
-  "[info]\t\thello-pepr: V1ConfigMap Binding created",
-  "[info]\t\thello-pepr: V1ConfigMap Binding action created",
+  "hello-pepr: V1ConfigMap Binding created",
+  "hello-pepr: V1ConfigMap Binding action created",
   "Server listening on port 3000",
 ];
 
@@ -22,7 +21,7 @@ function stripAnsiCodes(input) {
 }
 
 test.before(async t => {
-  const dir = path.resolve(__dirname, testDir);
+  const dir = path.resolve(testDir);
 
   try {
     await fs.access(dir);
@@ -30,14 +29,14 @@ test.before(async t => {
   } catch (err) {
     if (err.code !== "ENOENT") {
       throw err;
+      // The directory does not exist, do nothing
     }
-    // The directory does not exist, do nothing
   }
 });
 
 test("E2E: Pepr Init", async t => {
   try {
-    execSync("TEST_MODE=true pepr init", { stdio: "ignore" });
+    execSync("TEST_MODE=true pepr init", { stdio: "inherit" });
     t.pass();
   } catch (e) {
     t.fail(e.message);
@@ -46,7 +45,7 @@ test("E2E: Pepr Init", async t => {
 
 test("E2E: Pepr Build", async t => {
   try {
-    execSync("pepr build", { cwd: testDir, stdio: "ignore" });
+    execSync("pepr build", { cwd: testDir, stdio: "inherit" });
     t.pass();
   } catch (e) {
     t.fail(e.message);
@@ -54,55 +53,49 @@ test("E2E: Pepr Build", async t => {
 });
 
 test("E2E: Pepr Dev", async t => {
+  await t.notThrowsAsync(new Promise(peprDev));
+});
+
+function peprDev(resolve, reject) {
   const cmd = spawn("pepr", ["dev", "--confirm"], { cwd: testDir });
 
-  try {
-    await new Promise((resolve, reject) => {
-      cmd.stdout.on("data", data => {
-        // Convert buffer to string
-        data = stripAnsiCodes(data.toString());
-        console.log(data);
-        // Check if any expected lines are found
-        expectedLines = expectedLines.filter(expectedLine => {
-          const match = data.includes(expectedLine);
-          if (match) {
-            console.log(`Found expected line: ${expectedLine}`);
-          }
-          return !match;
-        });
+  cmd.stdout.on("data", data => {
+    // Convert buffer to string
+    data = stripAnsiCodes(data.toString());
+    console.log(data);
 
-        // If all expected lines are found, resolve the promise
-        if (expectedLines.length < 1) {
-          cmd.kill();
-          resolve();
-        }
-      });
-
-      // Log stderr
-      cmd.stderr.on("data", data => {
-        console.error(`stderr: ${data}`);
-      });
-
-      // This command should not exit on its own
-      cmd.on("close", code => {
-        reject(new Error(`Command exited with code ${code}`));
-      });
-
-      // Reject on error
-      cmd.on("error", error => {
-        reject(error);
-      });
-
-      // Reject on timeout
-      setTimeout(() => {
-        console.error("Remaining expected lines:" + JSON.stringify(expectedLines, null, 2));
-        cmd.kill();
-        reject(new Error("Timeout: Expected lines not found"));
-      }, TIMEOUT);
+    // Check if any expected lines are found
+    expectedLines = expectedLines.filter(expectedLine => {
+      // Check if the expected line is found in the output, ignoring whitespace
+      return !data.replace(/\s+/g, ' ').includes(expectedLine);
     });
 
-    t.pass();
-  } catch (error) {
-    t.fail(error.message);
-  }
-});
+    // If all expected lines are found, resolve the promise
+    if (expectedLines.length < 1) {
+      cmd.kill();
+      resolve();
+    }
+  });
+
+  // Log stderr
+  cmd.stderr.on("data", data => {
+    console.error(`stderr: ${data}`);
+  });
+
+  // This command should not exit on its own
+  cmd.on("close", code => {
+    reject(new Error(`Command exited with code ${code}`));
+  });
+
+  // Reject on error
+  cmd.on("error", error => {
+    reject(error);
+  });
+
+  // Reject on timeout
+  setTimeout(() => {
+    console.error("Remaining expected lines:" + JSON.stringify(expectedLines, null, 2));
+    cmd.kill();
+    reject(new Error("Timeout: Expected lines not found"));
+  }, TIMEOUT);
+}
