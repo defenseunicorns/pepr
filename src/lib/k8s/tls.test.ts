9af8c4882a0b8773a9c313c3ae07698c17726880
Case Wylie <cmwylie19@defenseunicorns.com>
2023-09-07 15:14:57 -0400

    format code
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/lib/k8s/tls.test.ts
+++ src/lib/k8s/tls.test.ts
@@ -4,16 +4,15 @@
 import { expect, test, describe } from "@jest/globals";
 import { genTLS } from "./tls";
 
-
 describe("tls", () => {
-    test("genTLS should generate a valid TLSOut object", () => {
-        const tls = genTLS("test");
-        expect(tls).toHaveProperty("ca");
-        expect(tls).toHaveProperty("crt");
-        expect(tls).toHaveProperty("key");
-        expect(tls).toHaveProperty("pem");
-        expect(tls.pem).toHaveProperty("ca");
-        expect(tls.pem).toHaveProperty("crt");
-        expect(tls.pem).toHaveProperty("key");
-    });
+  test("genTLS should generate a valid TLSOut object", () => {
+    const tls = genTLS("test");
+    expect(tls).toHaveProperty("ca");
+    expect(tls).toHaveProperty("crt");
+    expect(tls).toHaveProperty("key");
+    expect(tls).toHaveProperty("pem");
+    expect(tls.pem).toHaveProperty("ca");
+    expect(tls.pem).toHaveProperty("crt");
+    expect(tls.pem).toHaveProperty("key");
+  });
 });
