49844a24238132c194767cc3d3713f1ba084bdb7
Jeff McCoy <code@jeffm.us>
2023-09-14 02:18:04 -0500

    mock k8s client

---
+++ __mocks__/@kubernetes/client-node.ts
@@ -0,0 +1,22 @@
+// __mocks__/@kubernetes/client-node.ts
+
+import { jest } from "@jest/globals";
+
+const actual = jest.requireActual("@kubernetes/client-node") as any;
+
+const cloned = { ...actual };
+
+cloned.KubeConfig = class MockedKubeConfig {
+  loadFromDefault = jest.fn();
+
+  applyToRequest = jest.fn();
+
+  getCurrentCluster() {
+    return {
+      server: "http://jest-test:8080",
+    };
+  }
+};
+
+// export all elements of the mocked module
+module.exports = cloned;
