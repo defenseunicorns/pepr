5a521248800f1cae5cb18d188d602aeca98d2724
Case Wylie <cmwylie19@defenseunicorns.com>
2023-10-12 23:44:16 -0400

    perf: journey test for wasm should only test build step
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- journey/entrypoint-wasm.test.ts
+++ journey/entrypoint-wasm.test.ts
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-import { beforeAll, describe, jest } from "@jest/globals";
+import {  describe, jest } from "@jest/globals";
 
-import { before } from "./before-wasm";
 import { peprBuild } from "./pepr-build-wasm";
-import { peprInit } from "./pepr-init";
+
 
 // Unmock unit test things
 jest.deepUnmock("pino");
@@ -15,10 +14,5 @@ export const cwd = "pepr-test-module";
 
 // Allow 5 minutes for the tests to run
 jest.setTimeout(1000 * 60 * 5);
-
-// Configure the test environment before running the tests
-beforeAll(before);
-
-describe("Journey: `npx pepr init`", peprInit);
 
 describe("Journey: `npx pepr build -r gchr.io/defenseunicorns`", peprBuild);
