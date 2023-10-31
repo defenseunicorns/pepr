eeb423ae866dad23d6441cf1a270ba4f7a57b080
Case Wylie <cmwylie19@defenseunicorns.com>
2023-10-31 16:38:14 -0400

    chore: update test title
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- journey/entrypoint-wasm.test.ts
+++ journey/entrypoint-wasm.test.ts
@@ -15,4 +15,4 @@ export const cwd = "pepr-test-module";
 // Allow 5 minutes for the tests to run
 jest.setTimeout(1000 * 60 * 5);
 
-describe("Journey: `npx pepr build -r gchr.io/defenseunicorns --rbac-mode scoped -o dst`", peprBuild);
+describe("Journey: `npx pepr build -r gchr.io/defenseunicorns --rbac-mode scoped -o dist/pepr-test-module/child/folder`", peprBuild);
