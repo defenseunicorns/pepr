61a8f802357103b4e76069fea5cb53f74e161286
Megamind <882485+jeff-mccoy@users.noreply.github.com>
2023-11-03 13:01:25 -0500

    fix: include Ramda type data in prod deps for type-completion (#356)
    ## Description
    
    Force the inclusion of Ramda type data so Pepr users don't have to
    install the missing types.
    
    ## Related Issue
    
    Fixes #
    <!-- or -->
    Relates to #
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [ ] Test, docs, adr added or updated as needed
    - [ ] [Contributor Guide
    Steps](https://github.com/defenseunicorns/pepr/blob/main/CONTRIBUTING.md#submitting-a-pull-request)
    followed

--- src/lib.ts
+++ src/lib.ts
@@ -1,32 +1,25 @@
-import { K8s, RegisterKind, fetch, kind, kind as a, fetchStatus } from "kubernetes-fluent-client";
+import { K8s, RegisterKind, kind as a, fetch, fetchStatus, kind } from "kubernetes-fluent-client";
 import * as R from "ramda";
 
 import { Capability } from "./lib/capability";
 import Log from "./lib/logger";
 import { PeprModule } from "./lib/module";
 import { PeprMutateRequest } from "./lib/mutate-request";
-import { PeprValidateRequest } from "./lib/validate-request";
 import * as PeprUtils from "./lib/utils";
-
-// Import type information for external packages
-import type * as RTypes from "ramda";
+import { PeprValidateRequest } from "./lib/validate-request";
 
 export {
-  a,
-  kind,
-  /** PeprModule is used to setup a complete Pepr Module: `new PeprModule(cfg, {...capabilities})` */
+  Capability,
+  K8s,
+  Log,
   PeprModule,
   PeprMutateRequest,
-  PeprValidateRequest,
   PeprUtils,
-  RegisterKind,
-  K8s,
-  Capability,
-  Log,
+  PeprValidateRequest,
   R,
+  RegisterKind,
+  a,
   fetch,
   fetchStatus,
-
-  // Export the imported type information for external packages
-  RTypes,
+  kind,
 };
