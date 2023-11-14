e29e4324c195bd556f80dd1f3007f559d2bbccaf
Case Wylie <cmwylie19@defenseunicorns.com>
2023-11-14 15:06:39 -0500

    chore: clean up unused
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- journey/pepr-deploy.ts
+++ journey/pepr-deploy.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-import { describe, expect, it, jest } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import { execSync, spawnSync } from "child_process";
 import { K8s, kind } from "kubernetes-fluent-client";
 import { resolve } from "path";
