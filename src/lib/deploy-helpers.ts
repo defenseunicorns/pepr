0ed1bcd302d059625b95c62d46c24a7c0a265e66
Kim Schaefer <kim@defenseunicorns.com>
2023-12-12 13:02:53 -0700

    updated deploy-helpers logic

--- src/lib/deploy-helpers.ts
+++ src/lib/deploy-helpers.ts
@@ -4,29 +4,40 @@
 import { K8s, kind } from "kubernetes-fluent-client";
 import Log from "./logger";
 
-// waits for all replicas to be ready for all deployments in the pepr-system namespace
-export async function peprDeploymentsReady() {
+// check to see if all replicas are ready for all deployments in the pepr-system namespace
+// returns true if all deployments are ready, false otherwise
+export async function checkPeprDeploymentStatus() {
   const deployments = await K8s(kind.Deployment).InNamespace("pepr-system").Get();
   let status = false;
   let readyCount = 0;
 
   for (const deployment of deployments.items) {
-    while (deployment.status?.readyReplicas !== deployment.spec?.replicas) {
-      const readyReplicas = deployment.status?.readyReplicas ? deployment.status?.readyReplicas : 0;
+    const readyReplicas = deployment.status?.readyReplicas ? deployment.status?.readyReplicas : 0;
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    if (deployment.status?.readyReplicas !== deployment.spec?.replicas) {
       Log.info(
-        `Waiting for deployment ${deployment.metadata?.name} rollout to finish: ${readyReplicas} of ${deployment.spec?.replicas} replicas are available`,
+        `Waiting for deployment ${deployment.metadata?.name} rollout to finish: ${readyReplicas} of ${deployment.spec?.replicas} replicas are available`
       );
-      await new Promise(resolve => setTimeout(resolve, 5000));
+    } else {
+      Log.info(
+        `Deployment ${deployment.metadata?.name} rolled out: ${readyReplicas} of ${deployment.spec?.replicas} replicas are available`
+      );
+      readyCount++;
     }
-
-    Log.info(`Deployment ${deployment.metadata?.name} is sucessfully rolled out`);
-    readyCount++;
-    Log.info(`${readyCount} deployments ready out of ${deployments.items.length} deployments`);
   }
-
   if (readyCount === deployments.items.length) {
     status = true;
   }
-
   return status;
+}
+
+// wait for all deployments in the pepr-system namespace to be ready
+export async function peprDeploymentsReady() {
+  Log.info(`🔍 Checking pepr-system deployments status...`);
+  let ready = await checkPeprDeploymentStatus();
+  while (!ready) {
+    Log.info(`❌ Not all pepr-system deployments are ready`);
+    ready = await checkPeprDeploymentStatus();
+  }
+  Log.info(`✅ All pepr-system deployments are ready`);
 }
