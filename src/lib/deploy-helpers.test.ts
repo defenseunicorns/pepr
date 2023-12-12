057f8e9ec41b73b4ede27eb6f40b91b382606199
Kim Schaefer <kim@defenseunicorns.com>
2023-12-12 01:45:37 -0700

    updated tests to reflect updated deploy helper functions

--- src/lib/deploy-helpers.test.ts
+++ src/lib/deploy-helpers.test.ts
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-import { KubernetesListObject, kind } from "kubernetes-fluent-client";
+//import { KubernetesListObject, kind } from "kubernetes-fluent-client";
 import { beforeEach, describe, expect, it, jest } from "@jest/globals";
-import { waitForAllDeploymentReplicas } from "./deploy-helpers";
+import { peprDeploymentsReady } from "./deploy-helpers";
 
-const mockReadyDeployments = {
+/* const mockReadyDeployments = {
   items: [
     {
       metadata: {
@@ -79,44 +79,17 @@ const mockNotReadyDeployments = {
       },
     },
   ],
-} as KubernetesListObject<kind.Deployment>;
+} as KubernetesListObject<kind.Deployment>; */
 
 describe("deploy-helpers", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  describe("getDeployments", () => {
-    it("should get all deployments when all replicas are ready", async () => {
-      const mockGetDeployments = jest.fn().mockReturnValue(mockReadyDeployments);
-      const deployments = await mockGetDeployments();
-
-      expect(deployments).toEqual(mockReadyDeployments);
-    });
-
-    it("should get all deployments when some replicas are not ready", async () => {
-      const mockGetDeployments = jest.fn().mockReturnValue(mockNotReadyDeployments);
-      const deployments = await mockGetDeployments();
-
-      expect(deployments).toEqual(mockNotReadyDeployments);
-    });
-  });
-
-  describe("waitForAllDeploymentReplicas", () => {
+  describe("peprDeploymentsReady", () => {
     it("should return true when all replicas are ready", async () => {
-      const status = await waitForAllDeploymentReplicas(mockReadyDeployments);
-
+      const status = await peprDeploymentsReady();
       expect(status).toEqual(true);
-    });
-
-    it("should return false when there are no deployments", async () => {
-      const mockNoDeployments = {
-        items: [],
-      } as KubernetesListObject<kind.Deployment>;
-
-      const status = await waitForAllDeploymentReplicas(mockNoDeployments);
-
-      expect(status).toEqual(false);
     });
   });
 });
