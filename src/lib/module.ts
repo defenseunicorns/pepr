9afbc9cbe5e8f53ab18eff88611ce8825852e113
Case Wylie <cmwylie19@defenseunicorns.com>
2024-02-16 15:28:31 -0500

    feat: pepr store crd applied through kfc (#580)
    ## Description
    
    KFC to apply the PeprStore CRD to enable multiple Pepr modules in a helm
    package.
    
    I considered creating an ownerRef on the PeprStore CRD but then it
    became unclear which module would own the CRD. Since the point of this
    issue is to be able to deploy multiple modules it did not make sense.
    Although, it is not best practice to deploy multiple Pepr modules.
    Modules should be consolidated where possible
    https://docs.pepr.dev/main/best-practices/#multiple-modules-or-multiple-capabilities
    
    ## Related Issue
    
    Fixes #577
    
    <!-- or -->
    Relates to #
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [x] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [x] Test, docs, adr added or updated as needed
    - [x] [Contributor Guide
    Steps](https://github.com/defenseunicorns/pepr/blob/main/CONTRIBUTING.md#submitting-a-pull-request)
    followed
    
    ---------
    
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/lib/module.ts
+++ src/lib/module.ts
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
-
 import { clone } from "ramda";
-
 import { Capability } from "./capability";
 import { Controller } from "./controller";
 import { ValidateError } from "./errors";
