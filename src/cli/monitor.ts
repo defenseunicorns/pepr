5b660faeb723c44f3edaf18fdccfd06804a28630
Kim Schaefer <kim@defenseunicorns.com>
2024-02-23 05:02:07 -0700

    updated icon for mutation log

--- src/cli/monitor.ts
+++ src/cli/monitor.ts
@@ -59,7 +59,7 @@ export default function (program: RootCmd) {
               const uid = payload.uid;
 
               if (isMutate) {
-                const allowOrDeny = payload.res.allowed ? "🔀" : "❌";
+                const allowOrDeny = payload.res.allowed ? "🔀" : "❗";
                 console.log(`\n${allowOrDeny}  MUTATE     ${name} (${uid})`);
               } else {
                 const failures = Array.isArray(payload.res) ? payload.res : [payload.res];
