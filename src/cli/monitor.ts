a40c29d1e961c30a34da6d7010522eb06890738c
Kim Schaefer <kim@defenseunicorns.com>
2024-02-26 09:33:23 -0700

    updated mutation icon, color and info returned

--- src/cli/monitor.ts
+++ src/cli/monitor.ts
@@ -59,8 +59,14 @@ export default function (program: RootCmd) {
               const uid = payload.uid;
 
               if (isMutate) {
-                const allowOrDeny = payload.res.allowed ? "🔀" : "❗";
+                const patch = atob(payload.res.patch) || "";
+                const patchType = payload.res.patchType || payload.res.warnings || "";
+
+                const allowOrDeny = payload.res.allowed ? "🔀" : "🚫";
                 console.log(`\n${allowOrDeny}  MUTATE     ${name} (${uid})`);
+                if (patchType.length > 0) {
+                  console.log(`\n               \u001b[1;34m${patchType} ${patch}\u001b[0m`);
+                }
               } else {
                 const failures = Array.isArray(payload.res) ? payload.res : [payload.res];
 
@@ -70,7 +76,7 @@ export default function (program: RootCmd) {
                 // console.log(`${name} (${uid}) | VALIDATE | ${allow ? "ALLOW" : "DENY"}`);
                 if (filteredFailures.length > 0) {
                   console.log(`\n❌  VALIDATE   ${name} (${uid})`);
-                  console.debug(filteredFailures);
+                  console.debug(`               \u001b[1;31m${filteredFailures}\u001b[0m`);
                 } else {
                   console.log(`\n✅  VALIDATE   ${name} (${uid})`);
                 }
