a80f0fe1d9440867ba86d73da7bee21ffb1f0d35
Kim Schaefer <kim@defenseunicorns.com>
2024-03-21 11:08:15 -0600

    updated sanitizeResourceName

--- src/sdk.ts
+++ src/sdk.ts
@@ -34,6 +34,22 @@ export function getOwnerRef(request: PeprValidateRequest<a.Pod> | PeprMutateRequ
   return request.Raw.metadata?.ownerReferences || [];
 }
 
+/**
+ * Sanitize a resource name to make it a valid Kubernetes resource name.
+ *
+ * @param name the name of the resource to sanitize
+ * @returns the sanitized resource name
+ */
 export function sanitizeResourceName(name: string) {
-  return name;
+  return (
+    name
+      // The name must be lowercase
+      .toLowerCase()
+      // Replace sequences of non-alphanumeric characters with a single '-'
+      .replace(/[^a-z0-9]+/g, "-")
+      // Truncate the name to 250 characters
+      .slice(0, 250)
+      // Remove leading and trailing non-letter characters
+      .replace(/^[^a-z]+|[^a-z]+$/g, "")
+  );
 }
