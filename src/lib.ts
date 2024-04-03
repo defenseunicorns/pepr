42fa7388b904b80876baac7c2a0faffa1ba71996
Kim Schaefer <kim@defenseunicorns.com>
2024-04-03 13:33:02 -0600

    updated sdk imports

--- src/lib.ts
+++ src/lib.ts
@@ -7,6 +7,7 @@ import { PeprModule } from "./lib/module";
 import { PeprMutateRequest } from "./lib/mutate-request";
 import * as PeprUtils from "./lib/utils";
 import { PeprValidateRequest } from "./lib/validate-request";
+import * as sdk from "./sdk/sdk"
 
 export {
   Capability,
@@ -22,4 +23,5 @@ export {
   fetch,
   fetchStatus,
   kind,
+  sdk,
 };
