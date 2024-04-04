e7d9f09a717dc0cb7a1f4ad7ff2d3d760fdde0f3
Barrett LaFrance <barrett.t.lafrance@gmail.com>
2024-04-04 14:20:09 -0500

    ci: wip - parallelizing pepr-excellent-example test suite

--- .github/workflows/pepr-excellent-examples-matrix.js
+++ .github/workflows/pepr-excellent-examples-matrix.js
@@ -1,6 +1,18 @@
-const matrixList = ['package-a', 'package-b', 'package-c'].map((pkg) => ({
-  package: pkg,
-}));
+// const matrixList = ['package-a', 'package-b', 'package-c'].map((pkg) => ({
+//   package: pkg,
+// }));
 
-const includeStatement = { include: matrixList };
-console.log(`::set-output name=matrix::${JSON.stringify(includeStatement)}`);
+// const includeStatement = { include: matrixList };
+// console.log(`::set-output name=matrix::${JSON.stringify(includeStatement)}`);
+
+const examples = {
+  include: [{
+    nickname: "nick-example-a",
+    package: "pack-example-a"
+  },{
+    nickname: "nick-example-b",
+    package: "pack-example-b"
+  }]
+}
+
+console.log(`::set-output name=matrix::${JSON.stringify(examples)}`);
