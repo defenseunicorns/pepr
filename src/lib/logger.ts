e1165885547c12633c3d29946b70bfeefed12513
Case Wylie <cmwylie19@defenseunicorns.com>
2024-05-14 13:26:28 -0400

    chore: timestamp options
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -13,13 +13,13 @@ const pretty = {
 };
 
 const transport = isPrettyLog ? pretty : undefined;
-
+const pinoTimeStamp = process.env.PINO_TIME_STAMP === "iso" ? `,"time":"${new Date(Date.now()).toISOString()}"` : `,"time":"${Date.now()}"`;
 const Log = pino({
   transport,
+  timestamp: () => pinoTimeStamp
 });
 
 if (process.env.LOG_LEVEL) {
   Log.level = process.env.LOG_LEVEL;
 }
-
 export default Log;
