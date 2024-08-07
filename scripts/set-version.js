ee660d7b6dc3805d38ecd86be20f7f41e5225a81
Case Wylie <cmwylie19@defenseunicorns.com>
2024-08-07 13:44:39 -0400

    chore: scripts
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- scripts/set-version.js
+++ scripts/set-version.js
@@ -5,33 +5,38 @@
 
 const fs = require("fs");
 
-const packageJSON = require("../package.json")
-const packageLockJSON = require("../package-lock.json")
+const packageJSON = require("../package.json");
+const packageLockJSON = require("../package-lock.json");
 
-const version = process.env.GITHUB_REF_NAME;
+const generateVersion = () => {
+  let version = "";
+  version = process.env.GITHUB_REF_NAME.split("");
+  version = version[0] === "v" ? version.slice(1) : version;
+  return version.join("");
+};
 
-const packageJSONVersion = (packageJSON) => {
-    packageJSON.version = version;
-    writeToFile("./package.json", packageJSON);
-}
-const packageLockJSONVersion = (packageLockJSON) => {
-    packageLockJSON.version = version;
-    packageLockJSON.packages[""].version = version;
-    writeToFile("./package-lock.json", packageLockJSON);
-}
+const packageJSONVersion = (packageJSON, version) => {
+  packageJSON.version = version;
+  writeToFile("./package.json", packageJSON);
+};
+const packageLockJSONVersion = (packageLockJSON, version) => {
+  packageLockJSON.version = version;
+  packageLockJSON.packages[""].version = version;
+  writeToFile("./package-lock.json", packageLockJSON);
+};
 
 const writeToFile = (file, data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(file, JSON.stringify(data, null, 2), (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    })
-}
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, JSON.stringify(data, null, 2), err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
 
-
-packageJSONVersion(packageJSON);
-packageLockJSONVersion(packageLockJSON);
+const version = generateVersion();
+packageJSONVersion(packageJSON, version);
+packageLockJSONVersion(packageLockJSON, version);
