da618ca4d58a505c2ec1ea89a788ada94ac8ed35
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-09-05 10:15:46 -0500

    Refactor walkthrough() tests to parameterized tests

--- src/cli/init/walkthrough.test.ts
+++ src/cli/init/walkthrough.test.ts
@@ -1,58 +1,27 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-import { describe, expect, it, test } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import prompts from "prompts";
-import { walkthrough, confirm } from "./walkthrough";
+import { walkthrough, confirm, FinalPromptOptions } from "./walkthrough";
 
 describe("when processing input", () => {
-  test("walkthrough() returns expected results", async () => {
-    // Inject predefined answers for the prompts
-    prompts.inject(["My Test Module", "A test module for Pepr", 0]);
+  describe("walkthough() returns expected results", () => {
+    it.each(
+      [
+        ["description", ["My Test Module", "reject"]],
+        ["errorBehavior", ["My Test Module", "A test module for Pepr"]],
+        ["name", ["A test module for Pepr", "reject"]],
+        [undefined, ["My Test Module", "A test module for Pepr", "reject"]],
+  ]
+    )(`when the active flag is %s`, async (flagInput: string | undefined, promptInput) => {
 
-    const result = await walkthrough();
+    const allFlagsSet: FinalPromptOptions = {name: "My Test Module", description: "A test module for Pepr", errorBehavior: "reject"};
+    const activeFlag = Object.entries(allFlagsSet).filter(([key]) => key === flagInput).reduce((accumulator, [key, value]) => ({[key]: value}), {})
 
-    // Check the returned object
-    expect(result).toEqual({
-      name: "My Test Module",
-      description: "A test module for Pepr",
-      errorBehavior: 0,
-    });
-  });
+    prompts.inject(promptInput);
 
-  test("walkthrough() returns expected results - name only", async () => {
-    // Inject predefined answers for the prompts
-    prompts.inject(["A test module for Pepr", 0]);
-
-    const result = await walkthrough({ name: "My Test Module" });
-
-    // Check the returned object
-    expect(result).toEqual({
-      name: "My Test Module",
-      description: "A test module for Pepr",
-      errorBehavior: 0,
-    });
-  });
-
-  test("walkthrough() returns expected results - desc only", async () => {
-    // Inject predefined answers for the prompts
-    prompts.inject(["My Test Module", 0]);
-
-    const result = await walkthrough({ description: "A test module for Pepr" });
-
-    // Check the returned object
-    expect(result).toEqual({
-      name: "My Test Module",
-      description: "A test module for Pepr",
-      errorBehavior: 0,
-    });
-  });
-
-  test("walkthrough() returns expected results - errorBehavior only", async () => {
-    // Inject predefined answers for the prompts
-    prompts.inject(["My Test Module", "A test module for Pepr"]);
-
-    const result = await walkthrough({ errorBehavior: "reject" });
+    const result = await walkthrough(activeFlag);
 
     // Check the returned object
     expect(result).toEqual({
@@ -60,7 +29,8 @@ describe("when processing input", () => {
       description: "A test module for Pepr",
       errorBehavior: "reject", //TODO: Verify this is actually a string
     });
-  });
+    })
+  })
 
   describe("confirm() handles input", () =>{
     it.each(
