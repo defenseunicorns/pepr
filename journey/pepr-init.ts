9c1153fcfd5a7f05693026d8c9581658917c3342
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-09-10 15:28:17 -0500

    Extract common inputs to variables

--- journey/pepr-init.ts
+++ journey/pepr-init.ts
@@ -7,14 +7,16 @@ import { execSync } from "child_process";
 export function peprInit(){
   const peprAlias = "file:pepr-0.0.0-development.tgz";
   const moduleName = "pepr-test-module"
+  const description = "\"A test module for Pepr\""
+  const errorBehavior = "reject"
   
   function verifyModuleCreation(){
     const directoryContents = execSync(`ls -d ${moduleName}`)
     expect(directoryContents.toString()).toContain(`${moduleName}`)
-    const actualErrorBehavior = execSync(`grep reject "${moduleName}/package.json"`)
-    expect(actualErrorBehavior.toString()).toContain("reject")
-    const actualDescription = execSync(`grep asdf "${moduleName}/package.json"`)
-    expect(actualDescription.toString()).toContain("asdf")
+    const actualErrorBehavior = execSync(`grep ${errorBehavior} "${moduleName}/package.json"`)
+    expect(actualErrorBehavior.toString()).toContain(`${errorBehavior}`)
+    const actualDescription = execSync(`grep ${description} "${moduleName}/package.json"`)
+    expect(actualDescription.toString()).toContain(`${description}`)
   }
 
   it("should display the help menu", () => {
@@ -27,19 +29,19 @@ export function peprInit(){
 
   it("should create a new Pepr project with confirmation from STDIN", () => {
     execSync(`rm -rf ${moduleName}`)
-    execSync(`echo "y" | npx --yes ${peprAlias} init --name ${moduleName} --description asdf --errorBehavior reject`, { stdio:"inherit", encoding: 'utf8' });
+    execSync(`echo "y" | npx --yes ${peprAlias} init --name ${moduleName} --description ${description} --errorBehavior ${errorBehavior}`, { stdio:"inherit", encoding: 'utf8' });
     verifyModuleCreation()
   });
 
   it("should create a new Pepr project with prompt input from STDIN", () => {
     execSync(`rm -rf ${moduleName}`)
-    execSync(`echo "${moduleName}" | npx --yes ${peprAlias} init --description asdf --errorBehavior reject --confirm`, { stdio:"inherit", encoding: 'utf8' });
+    execSync(`echo "${moduleName}" | npx --yes ${peprAlias} init --description ${description} --errorBehavior ${errorBehavior} --confirm`, { stdio:"inherit", encoding: 'utf8' });
     verifyModuleCreation()
   });
 
   it("should create a new Pepr project using input flags", () => {
     execSync(`rm -rf ${moduleName}`)
-    execSync(`npx --yes ${peprAlias} init --name ${moduleName} --description asdf --errorBehavior reject --confirm`);
+    execSync(`npx --yes ${peprAlias} init --name ${moduleName} --description ${description} --errorBehavior ${errorBehavior} --confirm`);
     verifyModuleCreation()
   });
 }
