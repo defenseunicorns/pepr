7cdca9c39db5947360da35eb6b4aa59d7459ab4b
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-09-16 13:55:44 -0500

    Reorganize jest tests to use nested describe/it structure

--- journey/pepr-deploy.ts
+++ journey/pepr-deploy.ts
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-import { describe, expect, it } from "@jest/globals";
+import { beforeAll, describe, expect, it } from "@jest/globals";
 import { execSync, spawnSync, spawn } from "child_process";
 import { K8s, kind } from "kubernetes-fluent-client";
 import { resolve } from "path";
 import { destroyModule } from "../src/lib/assets/destroy";
 import { cwd } from "./entrypoint.test";
+import { promises as fs } from "fs"
 import {
   deleteConfigMap,
   noWaitPeprStoreKey,
@@ -17,13 +18,27 @@ import {
   waitForPeprStoreKey,
   waitForSecret,
 } from "./k8s";
-import nock from 'nock';
+
+async function getMagicString(): Promise<string> {
+  const files = fs.readdir(`${cwd}/dist`)
+  const yamlFile = (await files).find(file => /.*pepr-module.*\.yaml/.test(file))
+  const distSHA = yamlFile?.split('.')[0].split('-').slice(-5).join("-") as string; //TODO: Type coercion
+  const magicString = "pepr-".concat(distSHA)
+  return Promise.resolve(magicString);
+}
+
+let magicString = "unset";
 
 export function peprDeploy() {
-  // Purge the Pepr module from the cluster before running the tests
-  destroyModule("pepr-static-test");
 
-  it("should deploy the Pepr controller into the test cluster", async () => {
+  beforeAll(async ()=>{
+    magicString = await getMagicString();
+    console.log(`MAGIC STRING: ${magicString}`)
+    destroyModule(`${magicString}`);
+  })
+  // Purge the Pepr module from the cluster before running the tests
+
+  it.only("should deploy the Pepr controller into the test cluster", async () => {
     // Apply the store crd and pepr-system ns
     await applyStoreCRD();
 
@@ -37,168 +52,45 @@ export function peprDeploy() {
     execSync("npx pepr deploy -i pepr:dev --confirm", { cwd, stdio: "inherit" });
 
     // Wait for the deployments to be ready
-    await Promise.all([waitForDeploymentReady("pepr-system", "pepr-static-test"), waitForDeploymentReady("pepr-system", "pepr-static-test-watcher")]);
+    await Promise.all([waitForDeploymentReady("pepr-system", `${magicString}`), waitForDeploymentReady("pepr-system", `${magicString}-watcher`)]);
   });
 
   cleanupSamples();
 
-  describe("should ignore resources not defined in the capability namespace", testIgnore);
+  describe("should ignore resources not defined in the capability namespace", () => {
+    it("should ignore resources not in the capability namespaces during mutation", async () => {
+      const cm = await K8s(kind.ConfigMap).Apply({
+        metadata: {
+          name: "example-1",
+          namespace: "default",
+        },
+      });
+      expect(cm.metadata?.annotations?.["static-test.pepr.dev/hello-pepr"]).toBeUndefined();
+      expect(cm.metadata?.annotations?.["pepr.dev"]).toBeUndefined();
+      expect(cm.metadata?.labels?.["pepr"]).toBeUndefined();
+    });
+
+    it("should ignore resources not in the capability namespaces during validation", async () => {
+      const cm = await K8s(kind.ConfigMap).Apply({
+        metadata: {
+          name: "example-evil-cm",
+          namespace: "default",
+          annotations: {
+            evil: "true",
+          },
+        },
+      });
+      expect(cm.metadata?.annotations?.["static-test.pepr.dev/hello-pepr"]).toBeUndefined();
+      expect(cm.metadata?.annotations?.["pepr.dev"]).toBeUndefined();
+      expect(cm.metadata?.labels?.["pepr"]).toBeUndefined();
+    });
+  });
 
   it("should perform validation of resources applied to the test cluster", testValidate);
 
-  describe("should perform mutation of resources applied to the test cluster", testMutate);
+  describe("should perform mutation of resources applied to the test cluster", 
+    () => {
 
-  describe("should monitor the cluster for admission changes", () => {
-
-    const until = (predicate: () => boolean): Promise<void> => {
-      const poll = (resolve: () => void) => {
-        if (predicate()) { resolve() }
-        else { setTimeout(_ => poll(resolve), 250) }
-      }
-      return new Promise(poll);
-    }
-
-    it("npx pepr monitor should display validation results to console", async () => {
-      await testValidate();
-
-      const cmd = ['pepr', 'monitor', 'static-test']
-
-      const proc = spawn('npx', cmd, { shell: true })
-
-      const state = { accept: false, reject: false, done: false }
-      proc.stdout.on('data', (data) => {
-        const stdout: String = data.toString()
-        state.accept = stdout.includes("✅") ? true : state.accept
-        state.reject = stdout.includes("❌") ? true : state.reject
-        expect(stdout.includes("IGNORED")).toBe(false)
-        if (state.accept && state.reject) {
-          proc.kill()
-          proc.stdin.destroy()
-          proc.stdout.destroy()
-          proc.stderr.destroy()
-        }
-      })
-
-      proc.on('exit', () => state.done = true);
-
-      await until(() => state.done)
-
-      // completes only if conditions are met, so... getting here means success!
-    }, 10000);
-  });
-
-  describe("should display the UUIDs of the deployed modules", testUUID);
-
-  describe("should store data in the PeprStore", testStore);
-
-  cleanupSamples();
-}
-
-function cleanupSamples() {
-  try {
-    // Remove the sample yaml for the HelloPepr capability
-    execSync("kubectl delete -f hello-pepr.samples.yaml --ignore-not-found", {
-      cwd: resolve(cwd, "capabilities"),
-      stdio: "inherit",
-    });
-
-    deleteConfigMap("default", "example-1");
-    deleteConfigMap("default", "example-evil-cm");
-  } catch (e) {
-    // Ignore errors
-  }
-}
-
-function testUUID() {
-
-  it("should display the UUIDs of the deployed modules", async () => {
-    const uuidOut = spawnSync("npx pepr uuid", {
-      shell: true, // Run command in a shell
-      encoding: "utf-8", // Encode result as string
-    });
-
-    const { stdout } = uuidOut;
-
-    // Check if the expected lines are in the output
-    const expected = [
-      "UUID\t\tDescription",
-      "--------------------------------------------",
-      "static-test\t",
-    ].join("\n");
-    expect(stdout).toMatch(expected);
-  });
-
-  it("should display the UUIDs of the deployed modules with a specific UUID", async () => {
-    const uuidOut = spawnSync("npx pepr uuid static-test", {
-      shell: true, // Run command in a shell
-      encoding: "utf-8", // Encode result as string
-    });
-
-    const { stdout } = uuidOut;
-
-    // Check if the expected lines are in the output
-    const expected = [
-      "UUID\t\tDescription",
-      "--------------------------------------------",
-      "static-test\t",
-    ].join("\n");
-    expect(stdout).toMatch(expected);
-  });
-};
-
-function testIgnore() {
-  it("should ignore resources not in the capability namespaces during mutation", async () => {
-    const cm = await K8s(kind.ConfigMap).Apply({
-      metadata: {
-        name: "example-1",
-        namespace: "default",
-      },
-    });
-    expect(cm.metadata?.annotations?.["static-test.pepr.dev/hello-pepr"]).toBeUndefined();
-    expect(cm.metadata?.annotations?.["pepr.dev"]).toBeUndefined();
-    expect(cm.metadata?.labels?.["pepr"]).toBeUndefined();
-  });
-
-  it("should ignore resources not in the capability namespaces during validation", async () => {
-    const cm = await K8s(kind.ConfigMap).Apply({
-      metadata: {
-        name: "example-evil-cm",
-        namespace: "default",
-        annotations: {
-          evil: "true",
-        },
-      },
-    });
-    expect(cm.metadata?.annotations?.["static-test.pepr.dev/hello-pepr"]).toBeUndefined();
-    expect(cm.metadata?.annotations?.["pepr.dev"]).toBeUndefined();
-    expect(cm.metadata?.labels?.["pepr"]).toBeUndefined();
-  });
-}
-
-async function testValidate() {
-  // Apply the sample yaml for the HelloPepr capability
-  const applyOut = spawnSync("kubectl apply -f hello-pepr.samples.yaml", {
-    shell: true, // Run command in a shell
-    encoding: "utf-8", // Encode result as string
-    cwd: resolve(cwd, "capabilities"),
-  });
-
-  const { stderr, status } = applyOut;
-
-  // Validation should return an error
-  expect(status).toBe(1);
-
-  // Check if the expected lines are in the output
-  const expected = [
-    `Error from server: error when creating "hello-pepr.samples.yaml": `,
-    `admission webhook "pepr-static-test.pepr.dev" denied the request: `,
-    `No evil CM annotations allowed.\n`,
-  ].join("");
-  expect(stderr).toMatch(expected);
-
-}
-
-function testMutate() {
   it("should mutate the namespace", async () => {
     // Wait for the namespace to be created
     const ns = await waitForNamespace("pepr-demo");
@@ -269,32 +161,109 @@ function testMutate() {
       "VGhpcyBpcyBzb21lIHJhbmRvbSB0ZXh0OgoKICAgIC0gd2l0aCBsaW5lIGJyZWFrcwogICAgLSBhbmQgdGFicw==",
     );
   });
-}
+    });
+
+  it("should monitor the cluster for admission changes", () => {
+
+    const until = (predicate: () => boolean): Promise<void> => {
+      const poll = (resolve: () => void) => {
+        if (predicate()) { resolve() }
+        else { setTimeout(_ => poll(resolve), 250) }
+      }
+      return new Promise(poll);
+    }
+
+    it("npx pepr monitor should display validation results to console", async () => {
+      await testValidate();
+
+      const cmd = ['pepr', 'monitor', 'static-test']
+
+      const proc = spawn('npx', cmd, { shell: true })
+
+      const state = { accept: false, reject: false, done: false }
+      proc.stdout.on('data', (data) => {
+        const stdout: String = data.toString()
+        state.accept = stdout.includes("✅") ? true : state.accept
+        state.reject = stdout.includes("❌") ? true : state.reject
+        expect(stdout.includes("IGNORED")).toBe(false)
+        if (state.accept && state.reject) {
+          proc.kill()
+          proc.stdin.destroy()
+          proc.stdout.destroy()
+          proc.stderr.destroy()
+        }
+      })
+
+      proc.on('exit', () => state.done = true);
+
+      await until(() => state.done)
+
+      // completes only if conditions are met, so... getting here means success!
+    }, 10000);
+  });
+
+  describe("should display the UUIDs of the deployed modules", () => {
+
+    it("should display the UUIDs of the deployed modules", async () => {
+      const uuidOut = spawnSync("npx pepr uuid", {
+        shell: true, // Run command in a shell
+        encoding: "utf-8", // Encode result as string
+      });
+
+      const { stdout } = uuidOut;
+
+      // Check if the expected lines are in the output
+      const expected = [
+        "UUID\t\tDescription",
+        "--------------------------------------------",
+        "static-test\t",
+      ].join("\n");
+      expect(stdout).toMatch(expected);
+    });
+
+    it("should display the UUIDs of the deployed modules with a specific UUID", async () => {
+      const uuidOut = spawnSync("npx pepr uuid static-test", {
+        shell: true, // Run command in a shell
+        encoding: "utf-8", // Encode result as string
+      });
+
+      const { stdout } = uuidOut;
+
+      // Check if the expected lines are in the output
+      const expected = [
+        "UUID\t\tDescription",
+        "--------------------------------------------",
+        "static-test\t",
+      ].join("\n");
+      expect(stdout).toMatch(expected);
+    });
+  });
+
+  describe("should store data in the PeprStore", ()=>{
 
 
-function testStore() {
   it("should create the PeprStore", async () => {
-    const resp = await waitForPeprStoreKey("pepr-static-test-store", "__pepr_do_not_delete__");
+    const resp = await waitForPeprStoreKey(`${magicString}-store`, "__pepr_do_not_delete__");
     expect(resp).toBe("k-thx-bye");
   });
 
   it("should write the correct data to the PeprStore", async () => {
-    const key1 = await waitForPeprStoreKey("pepr-static-test-store", `hello-pepr-v2-example-1`);
+    const key1 = await waitForPeprStoreKey(`${magicString}-store`, `hello-pepr-v2-example-1`);
     expect(key1).toBe("was-here");
 
     // Should have been migrated and removed
-    const nullKey1 = await noWaitPeprStoreKey("pepr-static-test-store", `hello-pepr-example-1`);
+    const nullKey1 = await noWaitPeprStoreKey(`${magicString}-store`, `hello-pepr-example-1`);
     expect(nullKey1).toBeUndefined();
 
-    const key2 = await waitForPeprStoreKey("pepr-static-test-store", `hello-pepr-v2-example-1-data`);
+    const key2 = await waitForPeprStoreKey(`${magicString}-store`, `hello-pepr-v2-example-1-data`);
     expect(key2).toBe(JSON.stringify({ key: "ex-1-val" }));
 
     // Should have been migrated and removed
-    const nullKey2 = await noWaitPeprStoreKey("pepr-static-test-store", `hello-pepr-example-1-data`);
+    const nullKey2 = await noWaitPeprStoreKey(`${magicString}-store`, `hello-pepr-example-1-data`);
     expect(nullKey2).toBeUndefined();
 
     // Should have a key from the joke url and getItem should have worked
-    const key3 = await waitForPeprStoreKey("pepr-static-test-store", `hello-pepr-v2-https://icanhazdadjoke.com/`);
+    const key3 = await waitForPeprStoreKey(`${magicString}-store`, `hello-pepr-v2-https://icanhazdadjoke.com/`);
     expect(key3).toBeTruthy();
 
     const cm = await waitForConfigMapKey("pepr-demo", "example-5", "chuck-says");
@@ -303,9 +272,50 @@ function testStore() {
   });
 
   it("should write the correct data to the PeprStore from a Watch Action", async () => {
-    const key = await waitForPeprStoreKey("pepr-static-test-store", `hello-pepr-v2-watch-data`);
+    const key = await waitForPeprStoreKey(`${magicString}-store`, `hello-pepr-v2-watch-data`);
     expect(key).toBe("This data was stored by a Watch Action.");
   });
+
+  cleanupSamples();
+});
+
+function cleanupSamples() {
+  try {
+    // Remove the sample yaml for the HelloPepr capability
+    execSync("kubectl delete -f hello-pepr.samples.yaml --ignore-not-found", {
+      cwd: resolve(cwd, "capabilities"),
+      stdio: "inherit",
+    });
+
+    deleteConfigMap("default", "example-1");
+    deleteConfigMap("default", "example-evil-cm");
+  } catch (e) {
+    // Ignore errors
+  }
+}
+
+async function testValidate() {
+  
+  // Apply the sample yaml for the HelloPepr capability
+  const applyOut = spawnSync("kubectl apply -f hello-pepr.samples.yaml", {
+    shell: true, // Run command in a shell
+    encoding: "utf-8", // Encode result as string
+    cwd: resolve(cwd, "capabilities"),
+  });
+
+  const { stderr, status } = applyOut;
+
+  // Validation should return an error
+  expect(status).toBe(1);
+
+  // Check if the expected lines are in the output
+  const expected = [
+    `Error from server: error when creating "hello-pepr.samples.yaml": `,
+    `admission webhook "${magicString}.pepr.dev" denied the request: `,
+    `No evil CM annotations allowed.\n`,
+  ].join("");
+  expect(stderr).toMatch(expected);
+
 }
 
 async function applyStoreCRD() {
@@ -321,6 +331,11 @@ async function applyStoreCRD() {
 }
 
 async function applyLegacyStoreResource() {
+
+  // Prepare the store definition to match the pepr-test-module
+  execSync(`yq eval '.metadata.name = "${magicString}-store"' journey/resources/non-migrated-peprstore.yaml > temp.yaml`)
+  execSync(`cp journey/resources/non-migrated-peprstore.yaml journey/resources/non-migrated-peprstore.yaml.bak`)
+  execSync(`mv temp.yaml journey/resources/non-migrated-peprstore.yaml`)
   // Apply the store
   const appliedStore = spawnSync("kubectl apply -f journey/resources/non-migrated-peprstore.yaml", {
     shell: true, // Run command in a shell
@@ -329,5 +344,8 @@ async function applyLegacyStoreResource() {
   });
   const { stdout } = appliedStore;
 
-  expect(stdout).toContain("peprstore.pepr.dev/pepr-static-test-store");
+
+  execSync(`cp journey/resources/non-migrated-peprstore.yaml.bak journey/resources/non-migrated-peprstore.yaml`)
+  expect(stdout).toContain(`peprstore.pepr.dev/${magicString}-store`);
+}
 }
