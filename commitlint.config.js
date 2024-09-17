09de0fa7eade42a2718f6262368a6034960221c2
Barrett LaFrance <barrett.t.lafrance@gmail.com>
2024-09-17 15:34:28 -0500

    wip: update pattern

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   ignores: [
-    // prevent header-max-length error on long dependabot commits
+    // prevent header-max-length error on long, dependabot-gen'd commits titles
     //  https://github.com/dependabot/dependabot-core/issues/2445
-    (message) => /^.+: bump .+ from .+ to .+$/m.test(message)
+    (message) => /^chore: bump .+ from .+ to .+$/m.test(message)
   ],
 };
