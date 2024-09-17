69a85128bcc168da9e00c3d248a0517acb6b3613
Barrett LaFrance <barrett.t.lafrance@gmail.com>
2024-09-17 15:20:07 -0500

    chore: add commitlint rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [
+    // prevent header-max-length error on long dependabot commits
+    //  https://github.com/dependabot/dependabot-core/issues/2445
+    (message) => /^.+: bump .+ from .+ to .+\.$/m.test(message)
+  ],
+};
