d9478613a547ca49b89b1947bb765db115c27e13
Sam Mayer <samayer12@gmail.com>
2024-09-18 10:22:22 -0500

    feat(cli): support input flags for 'npx pepr init' (#1141)
    ## Description
    
    This PR is a version of #1116 without the overhaul to journey tests and
    does not remove `TEST_MODE`.
    
    ## Related Issue
    
    Fixes #1020 
    
    Relates to #1140 
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Checklist before merging
    - [x] Unit,
    [Journey](https://github.com/defenseunicorns/pepr/tree/main/journey),
    [E2E Tests](https://github.com/defenseunicorns/pepr-excellent-examples),
    [docs](https://github.com/defenseunicorns/pepr/tree/main/docs),
    [adr](https://github.com/defenseunicorns/pepr/tree/main/adr) added or
    updated as needed
    - [x] [Contributor Guide
    Steps](https://docs.pepr.dev/main/contribute/#submitting-a-pull-request)
    followed
    
    ---------
    
    Co-authored-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/cli/init/walkthrough.test.ts
+++ src/cli/init/walkthrough.test.ts
@@ -1,21 +1,102 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-import { expect, test } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import prompts from "prompts";
+import {
+  walkthrough,
+  confirm,
+  PromptOptions,
+  PartialPromptOptions,
+  setName,
+  setErrorBehavior,
+} from "./walkthrough";
 
-import { walkthrough } from "./walkthrough";
+describe("when processing input", () => {
+  describe("walkthough() returns expected results", () => {
+    it.each([
+      //Test flag combinations with [["$FLAG", ...]]
+      [["description", "errorBehavior"]],
+      [["description"]],
+      [["errorBehavior"]],
+      [["name", "description", "errorBehavior"]],
+      [["name", "description"]],
+      [["name", "errorBehavior"]],
+      [["name"]],
+      [undefined],
+    ])(`when the set flags are: %s`, async (flagInput: string[] | undefined) => {
+      const expected: PromptOptions = {
+        name: "My Test Module",
+        description: "A test module for Pepr",
+        errorBehavior: "reject",
+      };
 
-test("walkthrough() returns expected results", async () => {
-  // Inject predefined answers for the prompts
-  prompts.inject(["My Test Module", "A test module for Pepr", 0]);
+      // Set values for the flag(s) under test by making a subset of (expected)
+      type SupportedFlagNames = keyof typeof expected;
+      type PartialTestInput = { [key in SupportedFlagNames]?: string };
+      const setFlags =
+        flagInput?.reduce((acc: PartialTestInput, key: string) => {
+          if (key in expected) {
+            acc[key as SupportedFlagNames] = expected[key as SupportedFlagNames];
+          }
+          return acc;
+        }, {} as PartialTestInput) || {};
 
-  const result = await walkthrough();
+      // Simulate user-input for unset flags by making a subset of (expected - setFlags)
+      const promptInput = flagInput
+        ? Object.entries(expected)
+            .filter(([key]) => !flagInput.includes(key))
+            .map(([, value]) => value)
+        : Object.values(expected);
+      prompts.inject(Object.values(promptInput));
 
-  // Check the returned object
-  expect(result).toEqual({
-    name: "My Test Module",
-    description: "A test module for Pepr",
-    errorBehavior: 0,
+      const result = await walkthrough(setFlags as PartialPromptOptions);
+
+      expect(result).toEqual(expected);
+    });
+
+    it("should prompt for input when given invalid input", async () => {
+      const expected = { name: "aaa" };
+      prompts.inject(["aaa"]);
+      const result = await setName("aa");
+      expect(result).toStrictEqual(expected);
+    });
+
+    it("should prompt for errorBehavior when given invalid input", async () => {
+      const expected = { errorBehavior: "audit" };
+      prompts.inject(["audit"]);
+      const result = await setErrorBehavior("not-valid" as "reject"); // Type-Coercion forces invalid input
+      expect(result).toStrictEqual(expected);
+    });
+  });
+
+  describe("confirm() handles input", () => {
+    it.each([
+      ["n", false],
+      ["no", false],
+      ["y", true],
+      ["yes", true],
+    ])("when prompt input is '%s'", async (userInput: string, expected: boolean) => {
+      prompts.inject([userInput]);
+      const result = await confirm(
+        "some string",
+        { path: "some path", print: "some print" },
+        "some Pepr TS path",
+      );
+      expect(result).toBe(expected);
+    });
+
+    it.each([[true], [false]])(
+      "when flag '--confirm' is %s",
+      async (confirmFlag: boolean | undefined) => {
+        const result = await confirm(
+          "some string",
+          { path: "some path", print: "some print" },
+          "some Pepr TS path",
+          confirmFlag,
+        );
+        expect(result).toBe(confirmFlag);
+      },
+    );
   });
 });
