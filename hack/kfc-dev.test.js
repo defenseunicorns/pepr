50708e010af9c7ade928252184faba4aab2fcf3c
Case Wylie <cmwylie19@defenseunicorns.com>
2024-10-15 08:50:05 -0400

    chore: kfc automation scripts and workflow files (#1223)
    ## Description
    
    KFC Automation Script and Workflow files
    
    - [x] Run e2e/soak with KFC release candidate
    - [x] Local dev with with KFC release candidate, ie, `npx pepr dev`
    - [x] Bring in KFC dist and src to Pepr node_modules developing Pepr
    against new KFC feature
    
    ## Related Issue
    
    Fixes #1217
    <!-- or -->
    Relates to #
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [x] Other (security config, docs update, etc)
    
    ## Checklist before merging
    - [x] Unit,
    [Journey](https://github.com/defenseunicorns/pepr/tree/main/journey),
    [E2E Tests](https://github.com/defenseunicorns/pepr-excellent-examples),
    [docs](https://github.com/defenseunicorns/pepr/tree/main/docs),
    [adr](https://github.com/defenseunicorns/pepr/tree/main/adr) added or
    updated as needed
    - [x] [Contributor Guide
    Steps](https://docs.pepr.dev/main/contribute/#submitting-a-pull-request)
    followed
    
    ---------
    
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>
    Co-authored-by: Barrett <81570928+btlghrants@users.noreply.github.com>

---
+++ hack/kfc-dev.test.js
@@ -0,0 +1,85 @@
+const { expect } = require("@jest/globals");
+const { execSync } = require("node:child_process");
+const fs = require("fs");
+const { beforeEach } = require("node:test");
+
+describe("KFC Dev Helper E2E Test", () => {
+  beforeEach(() => {
+    try {
+      fs.rmSync("kubernetes-fluent-client", { recursive: true, force: true });
+      execSync("docker rmi pepr:dev");
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  afterAll(() => {
+    try {
+      fs.rmSync("kubernetes-fluent-client", { recursive: true, force: true });
+    } catch {}
+  });
+
+  it("should display usage information if no command is provided", () => {
+    try {
+      execSync("node hack/kfc-dev.js", {
+        stdio: "pipe",
+        encoding: "utf8",
+      });
+    } catch (error) {
+      expect(error.stderr.toString()).toContain("Usage:");
+    }
+  });
+
+  it("should fail if flag is missing for build", () => {
+    try {
+      execSync("node hack/kfc-dev.js build", {
+        stdio: "pipe",
+        encoding: "utf8",
+      });
+    } catch (error) {
+      expect(error.stderr.toString()).toContain("Please provide a path for the import operation.");
+    }
+  });
+
+  it("should fail if flag is missing for import", () => {
+    try {
+      execSync("node hack/kfc-dev.js import", {
+        stdio: "pipe",
+        encoding: "utf8",
+      });
+    } catch (error) {
+      expect(error.stderr.toString()).toContain("Please provide a path for the import operation.");
+    }
+  });
+
+  it("should download a remote branch of KFC to kubernetes-fluent-client folder", () => {
+    execSync("node hack/kfc-dev.js build -r main", {
+      stdio: "pipe",
+      encoding: "utf8",
+    });
+    expect(fs.existsSync("kubernetes-fluent-client")).toBe(true);
+
+    let dockerOutput = execSync("docker images");
+    expect(dockerOutput.toString()).toContain("pepr");
+  });
+
+  it("should import a local KFC to kubernetes-fluent-client folder", () => {
+    execSync("node hack/kfc-dev.js build -l ../../kubernetes-fluent-client", {
+      stdio: "pipe",
+      encoding: "utf8",
+    });
+    expect(fs.existsSync("kubernetes-fluent-client")).toBe(true);
+
+    let dockerOutput = execSync("docker images");
+    expect(dockerOutput.toString()).toContain("pepr");
+  });
+
+  it("should import a path of KFC to appropriate folders", () => {
+    execSync("node hack/kfc-dev.js import -l ../../kubernetes-fluent-client", {
+      stdio: "pipe",
+      encoding: "utf8",
+    });
+    expect(fs.existsSync("node_modules/kubernetes-fluent-client/src")).toBe(true);
+    expect(fs.existsSync("node_modules/kubernetes-fluent-client/dist")).toBe(true);
+  });
+});
