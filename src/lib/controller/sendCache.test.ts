e42354719688fd01d4ffaf6bc412708e6469c559
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-10-17 13:01:19 -0500

    Create blocks for adding/deleting cache entries

--- src/lib/controller/sendCache.test.ts
+++ src/lib/controller/sendCache.test.ts
@@ -3,19 +3,32 @@ import { fillCache, flushCache } from "./sendCache";
 import { Operation } from "fast-json-patch";
 
 describe("sendCache", () => {
-  it("should clear the cache", async () => {
-    const input: Record<string, Operation> = {
-      entry: { op: "remove", path: "/some/path" },
-      entry2: { op: "add", path: "some/path", value: "value" },
-    };
-    const result = await flushCache(input, "some namespace", "some name");
-    expect(result).toStrictEqual({ entry: undefined, entry2: undefined });
+  describe("when removing entries)", () => {
+    it("should clear the cache", async () => {
+      const input: Record<string, Operation> = {
+        entry: { op: "remove", path: "/some/path" },
+        entry2: { op: "add", path: "some/path", value: "value" },
+      };
+      const result = await flushCache(input, "some namespace", "some name");
+      expect(result).toStrictEqual({ entry: undefined, entry2: undefined });
+    });
   });
-  it("should write to the cache", () => {
-    const input: Record<string, Operation> = {
-      "add:/data/capability-key:value": { op: "add", path: "/data/capability-key", value: "value" },
-    };
-    const result = fillCache({}, "capability", "add", ["key"], "value");
-    expect(result).toStrictEqual(input);
+
+  describe("when adding entries)", () => {
+    it("should write to the cache", () => {
+      const input: Record<string, Operation> = {
+        "add:/data/capability-key:value": { op: "add", path: "/data/capability-key", value: "value" },
+      };
+      const result = fillCache({}, "capability", "add", ["key"], "value");
+      expect(result).toStrictEqual(input);
+    });
+
+    it("should write undefined values as empty-string", () => {
+      const input: Record<string, Operation> = {
+        "add:/data/capability-key:": { op: "add", path: "/data/capability-key", value: "" },
+      };
+      const result = fillCache({}, "capability", "add", ["key"], undefined);
+      expect(result).toStrictEqual(input);
+    });
   });
 });
