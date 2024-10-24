ad34c61cb9fadd856110950133669ca9b70bc5de
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-10-24 11:54:02 -0500

    Sort imports

--- src/lib/filter.ts
+++ src/lib/filter.ts
@@ -1,39 +1,40 @@
+/* eslint-disable complexity */
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
 import { AdmissionRequest, Binding, Operation } from "./types";
 import {
-  carriesIgnoredNamespace,
-  carriedName,
-  definedEvent,
-  declaredOperation,
-  definedName,
-  definedGroup,
-  declaredGroup,
-  definedVersion,
-  declaredVersion,
-  definedKind,
-  declaredKind,
-  definedNamespaces,
-  carriedNamespace,
-  definedLabels,
-  carriedLabels,
-  definedAnnotations,
   carriedAnnotations,
-  definedNamespaceRegexes,
+  carriedLabels,
+  carriedName,
+  carriedNamespace,
+  carriesIgnoredNamespace,
+  declaredGroup,
+  declaredKind,
+  declaredOperation,
+  declaredVersion,
+  definedAnnotations,
+  definedEvent,
+  definedGroup,
+  definedKind,
+  definedLabels,
+  definedName,
   definedNameRegex,
+  definedNamespaceRegexes,
+  definedNamespaces,
+  definedVersion,
   misboundDeleteWithDeletionTimestamp,
-  mismatchedDeletionTimestamp,
   mismatchedAnnotations,
+  mismatchedDeletionTimestamp,
+  mismatchedEvent,
+  mismatchedGroup,
+  mismatchedKind,
   mismatchedLabels,
   mismatchedName,
   mismatchedNameRegex,
   mismatchedNamespace,
   mismatchedNamespaceRegex,
-  mismatchedEvent,
-  mismatchedGroup,
   mismatchedVersion,
-  mismatchedKind,
   unbindableNamespaces,
   uncarryableNamespace,
 } from "./adjudicators";
@@ -54,90 +55,50 @@ export function shouldSkipRequest(
   const prefix = "Ignoring Admission Callback:";
   const obj = req.operation === Operation.DELETE ? req.oldObject : req.object;
 
-  // prettier-ignore
-  return (
-    misboundDeleteWithDeletionTimestamp(binding) ? 
-      `${prefix} Cannot use deletionTimestamp filter on a DELETE operation.` :
+  const admissionFilterMessage = misboundDeleteWithDeletionTimestamp(binding)
+    ? `${prefix} Cannot use deletionTimestamp filter on a DELETE operation.`
+    : mismatchedDeletionTimestamp(binding, obj)
+      ? `${prefix} Binding defines deletionTimestamp but Object does not carry it.`
+      : mismatchedEvent(binding, req)
+        ? `${prefix} Binding defines event '${definedEvent(binding)}' but ` +
+          `Request declares '${declaredOperation(req)}'.`
+        : mismatchedName(binding, obj)
+          ? `${prefix} Binding defines name '${definedName(binding)}' but Object carries '${carriedName(obj)}'.`
+          : mismatchedGroup(binding, req)
+            ? `${prefix} Binding defines group '${definedGroup(binding)}' but ` +
+              `Request declares '${declaredGroup(req)}'.`
+            : mismatchedVersion(binding, req)
+              ? `${prefix} Binding defines version '${definedVersion(binding)}' but ` +
+                `Request declares '${declaredVersion(req)}'.`
+              : mismatchedKind(binding, req)
+                ? `${prefix} Binding defines kind '${definedKind(binding)}' but ` +
+                  `Request declares '${declaredKind(req)}'.`
+                : unbindableNamespaces(capabilityNamespaces, binding)
+                  ? `${prefix} Binding defines namespaces ${JSON.stringify(definedNamespaces(binding))} ` +
+                    `but namespaces allowed by Capability are '${JSON.stringify(capabilityNamespaces)}'.`
+                  : uncarryableNamespace(capabilityNamespaces, obj)
+                    ? `${prefix} Object carries namespace '${carriedNamespace(obj)}' ` +
+                      `but namespaces allowed by Capability are '${JSON.stringify(capabilityNamespaces)}'.`
+                    : mismatchedNamespace(binding, obj)
+                      ? `${prefix} Binding defines namespaces '${JSON.stringify(definedNamespaces(binding))}' ` +
+                        `but Object carries '${carriedNamespace(obj)}'.`
+                      : mismatchedLabels(binding, obj)
+                        ? `${prefix} Binding defines labels '${JSON.stringify(definedLabels(binding))}' ` +
+                          `but Object carries '${JSON.stringify(carriedLabels(obj))}'.`
+                        : mismatchedAnnotations(binding, obj)
+                          ? `${prefix} Binding defines annotations '${JSON.stringify(definedAnnotations(binding))}' ` +
+                            `but Object carries '${JSON.stringify(carriedAnnotations(obj))}'.`
+                          : mismatchedNamespaceRegex(binding, obj)
+                            ? `${prefix} Binding defines namespace regexes ` +
+                              `'${JSON.stringify(definedNamespaceRegexes(binding))}' ` +
+                              `but Object carries '${carriedNamespace(obj)}'.`
+                            : mismatchedNameRegex(binding, obj)
+                              ? `${prefix} Binding defines name regex '${definedNameRegex(binding)}' ` +
+                                `but Object carries '${carriedName(obj)}'.`
+                              : carriesIgnoredNamespace(ignoredNamespaces, obj)
+                                ? `${prefix} Object carries namespace '${carriedNamespace(obj)}' ` +
+                                  `but ignored namespaces include '${JSON.stringify(ignoredNamespaces)}'.`
+                                : "";
 
-    mismatchedDeletionTimestamp(binding, obj) ?
-      `${prefix} Binding defines deletionTimestamp but Object does not carry it.` :
-
-    mismatchedEvent(binding, req) ?
-      (
-        `${prefix} Binding defines event '${definedEvent(binding)}' but ` +
-        `Request declares '${declaredOperation(req)}'.`
-      ) :
-
-    mismatchedName(binding, obj) ?
-      `${prefix} Binding defines name '${definedName(binding)}' but Object carries '${carriedName(obj)}'.` :
-
-    mismatchedGroup(binding, req) ?
-      (
-        `${prefix} Binding defines group '${definedGroup(binding)}' but ` +
-        `Request declares '${declaredGroup(req)}'.`
-      ) :
-
-    mismatchedVersion(binding, req) ?
-      (
-        `${prefix} Binding defines version '${definedVersion(binding)}' but ` +
-        `Request declares '${declaredVersion(req)}'.`
-      ) :
-
-    mismatchedKind(binding, req) ?
-      (
-        `${prefix} Binding defines kind '${definedKind(binding)}' but ` +
-        `Request declares '${declaredKind(req)}'.`
-      ) :
-
-    unbindableNamespaces(capabilityNamespaces, binding) ?
-      (
-        `${prefix} Binding defines namespaces ${JSON.stringify(definedNamespaces(binding))} ` +
-        `but namespaces allowed by Capability are '${JSON.stringify(capabilityNamespaces)}'.`
-      ) :
-
-    uncarryableNamespace(capabilityNamespaces, obj) ?
-      (
-        `${prefix} Object carries namespace '${carriedNamespace(obj)}' ` +
-        `but namespaces allowed by Capability are '${JSON.stringify(capabilityNamespaces)}'.`
-      ) :
-
-    mismatchedNamespace(binding, obj) ?
-      (
-        `${prefix} Binding defines namespaces '${JSON.stringify(definedNamespaces(binding))}' ` +
-        `but Object carries '${carriedNamespace(obj)}'.`
-      ) :
-
-    mismatchedLabels(binding, obj) ?
-      (
-        `${prefix} Binding defines labels '${JSON.stringify(definedLabels(binding))}' ` +
-        `but Object carries '${JSON.stringify(carriedLabels(obj))}'.`
-      ) :
-
-    mismatchedAnnotations(binding, obj) ?
-      (
-        `${prefix} Binding defines annotations '${JSON.stringify(definedAnnotations(binding))}' ` +
-        `but Object carries '${JSON.stringify(carriedAnnotations(obj))}'.`
-      ) :
-
-    mismatchedNamespaceRegex(binding, obj) ?
-      (
-        `${prefix} Binding defines namespace regexes ` +
-        `'${JSON.stringify(definedNamespaceRegexes(binding))}' ` +
-        `but Object carries '${carriedNamespace(obj)}'.`
-      ) :
-
-    mismatchedNameRegex(binding, obj) ?
-      (
-        `${prefix} Binding defines name regex '${definedNameRegex(binding)}' ` +
-        `but Object carries '${carriedName(obj)}'.`
-      ) :
-
-    carriesIgnoredNamespace(ignoredNamespaces, obj) ?
-      (
-        `${prefix} Object carries namespace '${carriedNamespace(obj)}' ` +
-        `but ignored namespaces include '${JSON.stringify(ignoredNamespaces)}'.`
-      ) :
-
-    ""
-  );
+  return admissionFilterMessage;
 }
