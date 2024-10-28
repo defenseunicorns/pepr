28fda9fada19c140c464e70ff3659e79e5348109
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-10-28 15:18:28 -0500

    Note some TODO items

--- src/lib/filter/filterChain.ts
+++ src/lib/filter/filterChain.ts
@@ -12,6 +12,8 @@ export class FilterChain {
     return this;
   }
   public execute(data: FilterParams): string {
+    //TODO: Break on first "failure"?
+    //TODO: Dev flag for detailed logging? Check some envar?
     return this.filters.reduce((result, filter) => {
       result += filter(data);
       // The result of each filter is passed as a new concatenated string
