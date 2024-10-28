b669f84e54fb8feb9d3fb2e8c422244263949fd5
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-10-28 15:37:22 -0500

    chore: refactor store code (#1259)
    ## Description
    
    Refactor the `store.ts` file.
    
    End to End Test:  
    (See [Pepr Excellent
    Examples](https://github.com/defenseunicorns/pepr-excellent-examples))
    
    ## Related Issue
    
    Fixes #959 
    <!-- or -->
    Relates to #
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [x] Other (security config, docs update, etc)
    
    ## Checklist before merging
    - [ ] Unit,
    [Journey](https://github.com/defenseunicorns/pepr/tree/main/journey),
    [E2E Tests](https://github.com/defenseunicorns/pepr-excellent-examples),
    [docs](https://github.com/defenseunicorns/pepr/tree/main/docs),
    [adr](https://github.com/defenseunicorns/pepr/tree/main/adr) added or
    updated as needed
    - [x] [Contributor Guide
    Steps](https://docs.pepr.dev/main/contribute/#submitting-a-pull-request)
    followed
    
    ---------
    
    Co-authored-by: Case Wylie <cmwylie19@defenseunicorns.com>
    Co-authored-by: Barrett <81570928+btlghrants@users.noreply.github.com>

---
+++ src/lib/controller/storeCache.ts
@@ -0,0 +1,63 @@
+import { DataOp } from "../storage";
+import Log from "../logger";
+import { K8s } from "kubernetes-fluent-client";
+import { Store } from "../k8s";
+import { StatusCodes } from "http-status-codes";
+import { Operation } from "fast-json-patch";
+
+export const sendUpdatesAndFlushCache = async (cache: Record<string, Operation>, namespace: string, name: string) => {
+  const indexes = Object.keys(cache);
+  const payload = Object.values(cache);
+
+  try {
+    if (payload.length > 0) {
+      await K8s(Store, { namespace, name }).Patch(payload); // Send patch to cluster
+      Object.keys(cache).forEach(key => delete cache[key]);
+    }
+  } catch (err) {
+    Log.error(err, "Pepr store update failure");
+
+    if (err.status === StatusCodes.UNPROCESSABLE_ENTITY) {
+      Object.keys(cache).forEach(key => delete cache[key]);
+    } else {
+      indexes.forEach(index => {
+        cache[index] = payload[Number(index)]; // On failure to update, re-add the operations to the cache to be retried
+      });
+    }
+  }
+  return cache;
+};
+
+type CacheItem = {
+  key: string[];
+  value?: string;
+  version?: string;
+};
+
+export const fillStoreCache = (
+  cache: Record<string, Operation>,
+  capabilityName: string,
+  op: DataOp,
+  cacheItem: CacheItem,
+): Record<string, Operation> => {
+  const path = [`/data/${capabilityName}`, cacheItem.version, cacheItem.key] // adjust the path, see ADR-0008
+    .filter(str => str !== "" && str !== undefined)
+    .join("-");
+  if (op === "add") {
+    const value = cacheItem.value || "";
+    const cacheIdx = [op, path, value].join(":");
+
+    // Add the operation to the cache
+    cache[cacheIdx] = { op, path, value };
+  } else if (op === "remove") {
+    if (cacheItem.key.length < 1) {
+      throw new Error(`Key is required for REMOVE operation`);
+    }
+    const cacheIndex = [op, path].join(":");
+    // Add the operation to the cache
+    cache[cacheIndex] = { op, path };
+  } else {
+    throw new Error(`Unsupported operation: ${op}`);
+  }
+  return cache;
+};
