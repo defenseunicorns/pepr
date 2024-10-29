3903fee481a8b1dc4484a7b6d8387608c3157352
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-10-29 12:12:57 -0500

    Stop filtering after first failure

--- src/lib/filter/filterChain.ts
+++ src/lib/filter/filterChain.ts
@@ -12,12 +12,13 @@ export class FilterChain {
     return this;
   }
   public execute(data: FilterParams): string {
-    //TODO: Break on first "failure"?
-    //TODO: Dev flag for detailed logging? Check some envar?
-    return this.filters.reduce((result, filter) => {
+    let result = "";
+    for (const filter of this.filters) {
       result += filter(data);
-      // The result of each filter is passed as a new concatenated string
-      return result;
-    }, "");
+      if (result !== "") {
+        break;
+      }
+    }
+    return result;
   }
 }
