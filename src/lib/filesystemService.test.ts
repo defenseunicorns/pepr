77c8b337a7fe8150673e8d61942be83c03a71f74
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-11-22 11:10:35 -0600

    Add license

--- src/lib/filesystemService.test.ts
+++ src/lib/filesystemService.test.ts
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023-Present The Pepr Authors
+
 import { describe, jest, expect, it } from "@jest/globals";
 import { createDirectoryIfNotExists } from "./filesystemService";
 import { promises as fs } from "fs";
