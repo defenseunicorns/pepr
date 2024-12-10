f4802f5c8eae8bc2d4f52c0bc29d2f1c6f1378eb
Barrett LaFrance <barrett.t.lafrance@gmail.com>
2024-12-10 16:22:17 -0600

    test: gettin' coverage

--- src/lib/mutate-processor.test.ts
+++ src/lib/mutate-processor.test.ts
@@ -2,114 +2,70 @@
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
 import { describe, expect, it } from "@jest/globals";
-// import { GroupVersionKind, kind, KubernetesObject } from "kubernetes-fluent-client";
-// import { AdmissionRequest, Binding, Filters } from "./types";
-// import { Event, Operation } from "./enums";
-// import { PeprValidateRequest } from "./validate-request";
-// import { clone } from "ramda";
-// import * as sut from "./mutate-processor";
+import { clone } from "ramda";
+import { ModuleConfig } from "./module";
+import { PeprMutateRequest } from "./mutate-request";
+import * as sut from "./mutate-processor";
+import { AdmissionRequest } from "./types";
+import { Operation } from "./enums";
 
-// const testFilters: Filters = {
-//   annotations: {},
-//   deletionTimestamp: false,
-//   labels: {},
-//   name: "",
-//   namespaces: [],
-//   regexName: "^default$",
-//   regexNamespaces: [] as string[],
-// };
+const defaultModuleConfig: ModuleConfig = {
+  uuid: "test-uuid",
+  alwaysIgnore: {},
+};
 
-// const testGroupVersionKind: GroupVersionKind = {
-//   kind: "some-kind",
-//   group: "some-group",
-// };
+const defaultAdmissionRequest: AdmissionRequest = {
+  uid: "uid",
+  kind: {
+    kind: "kind",
+    group: "group",
+  },
+  resource: {
+    group: "group",
+    version: "version",
+    resource: "resource",
+  },
+  name: "",
+  object: {},
+  operation: Operation.CREATE,
+  userInfo: {},
+};
 
-// const testBinding: Binding = {
-//   event: Event.ANY,
-//   filters: testFilters,
-//   kind: testGroupVersionKind,
-//   model: kind.Pod,
-//   isFinalize: false,
-//   isMutate: false,
-//   isQueue: false,
-//   isValidate: false,
-//   isWatch: false,
-// };
-
-// export const testAdmissionRequest: AdmissionRequest = {
-//   uid: "some-uid",
-//   kind: { kind: "a-kind", group: "a-group" },
-//   resource: { group: "some-group", version: "some-version", resource: "some-resource" },
-//   operation: Operation.CONNECT,
-//   name: "some-name",
-//   userInfo: {},
-//   object: {},
-// };
-
-// export const testActionMetadata: Record<string, string> = {};
-
-// export const testPeprValidateRequest = (admissionRequest: AdmissionRequest) =>
-//   new PeprValidateRequest<KubernetesObject>(admissionRequest);
+const defaultPeprMutateRequest = (admissionRequest = defaultAdmissionRequest) =>
+  new PeprMutateRequest(admissionRequest);
 
 describe("updateStatus", () => {
-  it("does...", () => {
-    expect(true).toBe(true);
+  describe("when given non-delete request", () => {
+    it("adds status annotation to to-be-admitted resource", () => {
+      const name = "capa";
+      const status = "test-status";
+      const annote = `${defaultModuleConfig.uuid}.pepr.dev/${name}`;
+
+      const result = sut.updateStatus(defaultModuleConfig, name, defaultPeprMutateRequest(), status);
+
+      expect(result.HasAnnotation(annote)).toBe(true);
+      expect(result.Raw.metadata?.annotations?.[annote]).toBe(status);
+    });
+  });
+
+  describe("when given delete request", () => {
+    it("does not add status annotation to to-be-admitted resource", () => {
+      const testAdmissionRequest = {
+        ...clone(defaultAdmissionRequest),
+        operation: Operation.DELETE,
+        oldObject: {},
+      };
+      const name = "capa";
+      const annote = `${defaultModuleConfig.uuid}.pepr.dev/${name}`;
+
+      const result = sut.updateStatus(
+        defaultModuleConfig,
+        name,
+        defaultPeprMutateRequest(testAdmissionRequest),
+        "test-status",
+      );
+
+      expect(result.HasAnnotation(annote)).toBe(false);
+    });
   });
 });
-
-describe("logMutateErrorMessage", () => {
-  it("does...", () => {
-    expect(true).toBe(true);
-  });
-});
-
-// describe("processRequest", () => {
-//   let binding: Binding;
-//   let actionMetadata: Record<string, string>;
-//   let peprValidateRequest: PeprValidateRequest<KubernetesObject>;
-
-//   beforeEach(() => {
-//     binding = clone(testBinding);
-//     actionMetadata = clone(testActionMetadata);
-//     peprValidateRequest = testPeprValidateRequest(testAdmissionRequest);
-//   });
-
-//   it("responds on successful validation action", async () => {
-//     const cbResult = {
-//       allowed: true,
-//       statusCode: 200,
-//       statusMessage: "yay",
-//     };
-//     const callback = jest.fn().mockImplementation(() => cbResult) as Binding["validateCallback"];
-//     binding = { ...clone(testBinding), validateCallback: callback };
-
-//     const result = await sut.processRequest(binding, actionMetadata, peprValidateRequest);
-
-//     expect(result).toEqual({
-//       uid: peprValidateRequest.Request.uid,
-//       allowed: cbResult.allowed,
-//       status: {
-//         code: cbResult.statusCode,
-//         message: cbResult.statusMessage,
-//       },
-//     });
-//   });
-
-//   it("responds on unsuccessful validation action", async () => {
-//     const callback = jest.fn().mockImplementation(() => {
-//       throw "oof";
-//     }) as Binding["validateCallback"];
-//     binding = { ...clone(testBinding), validateCallback: callback };
-
-//     const result = await sut.processRequest(binding, actionMetadata, peprValidateRequest);
-
-//     expect(result).toEqual({
-//       uid: peprValidateRequest.Request.uid,
-//       allowed: false,
-//       status: {
-//         code: 500,
-//         message: `Action failed with error: "oof"`,
-//       },
-//     });
-//   });
-// });
