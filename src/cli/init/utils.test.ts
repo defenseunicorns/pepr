86edbf89d9461082763696c5eac2e3dc56edcf47
Case Wylie <cmwylie19@defenseunicorns.com>
2024-12-16 17:02:41 -0500

    chore: change back - should be in a different pr
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/cli/init/utils.test.ts
+++ src/cli/init/utils.test.ts
@@ -1,16 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-import { expect, it, jest, describe, afterEach } from "@jest/globals";
-import { promises as fs } from "fs";
-import { sanitizeName, createDir, write } from "./utils";
+import { expect, it } from "@jest/globals";
 
-jest.mock("fs", () => ({
-  promises: {
-    mkdir: jest.fn(),
-    writeFile: jest.fn(),
-  },
-}));
+import { sanitizeName } from "./utils";
 
 it.each([
   //Test sanitizeName() with ["$BAD_INPUT", "$SANITIZED_INPUT"]
@@ -23,60 +16,4 @@ it.each([
 
 it("sanitizeName() should throw TypeError when given a non-string", () => {
   expect(() => sanitizeName({ input: 0 } as unknown as string)).toThrow(TypeError);
-});
-
-describe("createDir", () => {
-  const mockedMkdir = fs.mkdir as jest.Mock;
-
-  afterEach(() => {
-    jest.clearAllMocks(); // Clear mocks between tests
-  });
-
-  it("should create the pepr-test-module dir", async () => {
-    await createDir("pepr-test-module");
-    expect(mockedMkdir).toHaveBeenCalledWith("pepr-test-module");
-  });
-
-  it("should throw an error if the directory already exists", async () => {
-    const error = new Error("Directory already exists") as NodeJS.ErrnoException;
-    error.code = "EEXIST";
-    mockedMkdir.mockRejectedValueOnce(error as unknown as never);
-
-    await expect(createDir("pepr-test-module")).rejects.toThrow(
-      "Directory pepr-test-module already exists",
-    );
-    expect(mockedMkdir).toHaveBeenCalledTimes(1);
-    expect(mockedMkdir).toHaveBeenCalledWith("pepr-test-module");
-  });
-
-  it("should throw the error if there is another error other than already exists", async () => {
-    const error = new Error("Directory already exists") as NodeJS.ErrnoException;
-    error.code = "ENOENT";
-    mockedMkdir.mockRejectedValueOnce(error as unknown as never);
-
-    await expect(createDir("pepr-test-module")).rejects.toThrow(error);
-    expect(mockedMkdir).toHaveBeenCalledTimes(1);
-    expect(mockedMkdir).toHaveBeenCalledWith("pepr-test-module");
-  });
-});
-
-describe("write", () => {
-  const mockedWriteFile = fs.writeFile as jest.Mock;
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it("should write data to a file", async () => {
-    await write("package.json", "pepr-test-module");
-    expect(mockedWriteFile).toHaveBeenCalledWith("package.json", "pepr-test-module");
-  });
-
-  it("should stringify data if it is not a string", async () => {
-    await write("package.json", { name: "pepr-test-module" });
-    expect(mockedWriteFile).toHaveBeenCalledWith(
-      "package.json",
-      JSON.stringify({ name: "pepr-test-module" }, null, 2),
-    );
-  });
 });
