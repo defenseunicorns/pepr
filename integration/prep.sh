33debcca2211641745607213f3f2da5ff2f82e2f
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-01-21 15:18:04 -0600

    Use supported flag in mkdir

--- integration/prep.sh
+++ integration/prep.sh
@@ -9,7 +9,7 @@ HERE="$(dirname "$ME")"
 ROOT="$(dirname "$HERE")"
 
 export NPM_CONFIG_CACHE="${HERE}/testroot/.npm"
-mkdir --parents "$NPM_CONFIG_CACHE"
+mkdir -p "$NPM_CONFIG_CACHE"
 
 npm run build
 npx --yes file://${ROOT}/pepr-0.0.0-development.tgz
