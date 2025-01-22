cb7aeb6857768a2a2cea242a5a091fcf06c85b55
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-01-22 11:56:29 -0600

    Remove testing scripts

--- integration/process-logs.sh
+++
@@ -1,6 +0,0 @@
-for file in timeout-logs/*.json; do
-    echo "$(jq '.numFailedTests' "$file")" 
-done  > extracted_numFailedTests.txt
-
-echo "Freq|# of Failing Tests"
-sort extracted_numFailedTests.txt | uniq -c
