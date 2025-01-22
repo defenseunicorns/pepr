c93452da6477c4b753f97a2d8a2bcc6b8f4fe549
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-01-22 13:03:01 -0600

    fix: local integration testing setup/timeouts (#1705)
    ## Description
    
    In support of #1661, this PR fixes issues with test setup for
    integration tests.
    
    End to End Test:  <!-- if applicable -->  
    (See [Pepr Excellent
    Examples](https://github.com/defenseunicorns/pepr-excellent-examples))
    
    ## Related Issue
    
    Relates to #1661
    
    ## Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Checklist before merging
    - [x] Unit,
    [Journey](https://github.com/defenseunicorns/pepr/tree/main/journey),
    [E2E Tests](https://github.com/defenseunicorns/pepr-excellent-examples),
    [docs](https://github.com/defenseunicorns/pepr/tree/main/docs),
    [adr](https://github.com/defenseunicorns/pepr/tree/main/adr) added or
    updated as needed
    - [x] [Contributor Guide
    Steps](https://docs.pepr.dev/main/contribute/#submitting-a-pull-request)
    followed
    
    ---------
    
    Co-authored-by: Barrett <81570928+btlghrants@users.noreply.github.com>

--- integration/prep.sh
+++ integration/prep.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 # This script makes a version of the npm cache for local use to avoid installing test artifacts into the global npm cache.
 # This isn't an issue in CI where environments are ephemeral, but is useful for local testing.
+
+set -euo pipefail
+
 ME="$(readlink -f "$0")"
 HERE="$(dirname "$ME")"
 ROOT="$(dirname "$HERE")"
 
 export NPM_CONFIG_CACHE="${HERE}/testroot/.npm"
-mkdir --parents "$NPM_CONFIG_CACHE"
+# ubuntu supports "--parents" long opt but macOS does not, so using "-p" short opt
+mkdir -p "$NPM_CONFIG_CACHE"
 
 npm run build
 npx --yes file://${ROOT}/pepr-0.0.0-development.tgz
