6b6dbfc434ab45818bc8fe375f51028c5e7ae9b6
Case Wylie <cmwylie19@defenseunicorns.com>
2025-01-29 15:08:19 -0500

    chore: quick fix on npm install and npm build for nightly (#1743)
    ## Description
    
    Needs to be installed and build before pushing to registry
    
    ## Related Issue
    
    Fixes #
    <!-- or -->
    Relates to #
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [x] Other (security config, docs update, etc)
    
    ## Checklist before merging
    - [x] Unit,
    [Journey](https://github.com/defenseunicorns/pepr/tree/main/journey),
    [E2E Tests](https://github.com/defenseunicorns/pepr-excellent-examples),
    [docs](https://github.com/defenseunicorns/pepr/tree/main/docs),
    [adr](https://github.com/defenseunicorns/pepr/tree/main/adr) added or
    updated as needed
    - [x] [Contributor Guide
    Steps](https://docs.pepr.dev/main/contribute/#submitting-a-pull-request)
    followed
    
    ---------
    
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- scripts/nightlies.sh
+++ scripts/nightlies.sh
@@ -35,4 +35,7 @@ npm version --no-git-tag-version "$FULL_VERSION"
 
 docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag ghcr.io/defenseunicorns/pepr/controller:v"$FULL_VERSION" .
 
+npm install 
+npm run build
+
 npm publish --tag "nightly"
