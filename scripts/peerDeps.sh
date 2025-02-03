0383a035c9f3c331987ffe85fba4b9a5181ae90f
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-02-03 12:16:02 -0600

    Create script to make updating peerDeps less tedious

---
+++ scripts/peerDeps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Reviewing peerDependencies:"
+echo ""
+PEER_DEPS=$(jq -r '.peerDependencies | keys[]' package.json)
+
+for dependency in $PEER_DEPS; do
+  echo "\"$dependency\": \"$(npm show "$dependency" version)\","
+done
+
+echo ""
+echo "INFO - Manually copy these values to update the listed versions in package.json's peerDependencies."
