fe34b64cadbb02cc41efc88c6f7099a1d5effe79
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-02-11 11:34:22 -0600

    Update assertions to account for breaking change in esbuild:0.25.0

--- integration/cli/build.noembed.test.ts
+++ integration/cli/build.noembed.test.ts
@@ -53,22 +53,30 @@ describe("build", () => {
     describe("for use as a library", () => {
       const packageJson = resource.fromFile(`${testModule}/package.json`);
       const uuid = packageJson.pepr.uuid;
-
-      it.each([
-        [`pepr-${uuid}.js`],
+      const omittedConfigFiles = [
+        [`${uuid}-chart/`],
         [`pepr-${uuid}.js.map`],
-        [`pepr-${uuid}.js.LEGAL.txt`],
+        [`pepr-${uuid}.js`],
         [`pepr-module-${uuid}.yaml`],
         [`zarf.yaml`],
-        [`${uuid}-chart/`],
-      ])("should not create configuration file: '%s'", filename => {
+      ];
+
+      it.each(omittedConfigFiles)("should not create configuration file: '%s'", filename => {
         expect(existsSync(`${testModule}/dist/${filename}`)).toBe(false);
       });
 
-      it.each([[`pepr.js`], [`pepr.js.map`], [`pepr.js.LEGAL.txt`]])(
+      it.each([[`pepr.d.ts.map`], [`pepr.d.ts`], [`pepr.js.map`], [`pepr.js`]])(
         "should create configuration file: '%s'",
         filename => {
           expect(existsSync(`${testModule}/dist/${filename}`)).toBe(true);
+        },
+      );
+
+      it.each([[`pepr-${uuid}.js.LEGAL.txt`], [`pepr.js.LEGAL.txt`]])(
+        "should not create legal file: '%s'",
+        filename => {
+          // Omitted when empty, see esbuild/#3670 https://github.com/evanw/esbuild/blob/main/CHANGELOG.md#0250
+          expect(existsSync(`${testModule}/dist/${filename}`)).toBe(false);
         },
       );
     });
