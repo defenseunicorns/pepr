25f171064f1a0d89e68bf4b292796d28b7a2ba41
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-02-12 15:12:40 -0600

    Prefer it() over test() for BDD-style test names

--- src/lib/included-files.test.ts
+++ src/lib/included-files.test.ts
@@ -2,14 +2,14 @@
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
 import { createDockerfile } from "./included-files";
-import { expect, describe, test } from "@jest/globals";
+import { expect, describe, it } from "@jest/globals";
 import { promises as fs } from "fs";
 
 describe("createDockerfile", () => {
   const version = "0.0.1";
   const description = "Pepr supports WASM modules!";
   const includedFiles = ["main.wasm", "wasm_exec.js"];
-  test("should create a Dockerfile.controller with the correct content", async () => {
+  it("should create a Dockerfile.controller with the correct content", async () => {
     await createDockerfile(version, description, includedFiles);
 
     const generatedContent = await fs.readFile("Dockerfile.controller", "utf-8");
--- src/lib/tls.test.ts
+++ src/lib/tls.test.ts
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-import { expect, test, describe } from "@jest/globals";
+import { expect, it, describe } from "@jest/globals";
 import { genTLS } from "./tls";
 
 describe("tls", () => {
-  test("genTLS should generate a valid TLSOut object", () => {
+  it("genTLS should generate a valid TLSOut object", () => {
     const tls = genTLS("test");
     expect(tls).toHaveProperty("ca");
     expect(tls).toHaveProperty("crt");
