70e34b90c6f58da50010455ef70ce896dc694e72
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-03-10 10:48:56 -0500

    Remove unused import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
