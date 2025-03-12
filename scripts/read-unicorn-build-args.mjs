0474cf3a9120ae9146ce05925a5a33fdfcee0f93
Case Wylie <cmwylie19@defenseunicorns.com>
2025-03-12 16:43:03 -0400

    chore: rename script
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- scripts/read-unicorn-build-args.mjs
+++ scripts/read-unicorn-build-args.mjs
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present The Pepr Authors
 
-const { readFileSync } = require("fs");
-const { load } = require("js-yaml");
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
 
 try {
   const config = load(readFileSync("./config/unicorn.yaml", "utf8"));
