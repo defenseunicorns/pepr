11cfdbb651ae30012c77bb8ae24030f9d0ab0aee
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-04-03 11:19:48 -0500

    Use portable syntax for bash array

--- scripts/nightlies.sh
+++ scripts/nightlies.sh
@@ -35,8 +35,11 @@ npm version --no-git-tag-version "$FULL_VERSION"
 
 docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag ghcr.io/defenseunicorns/pepr/controller:v"$FULL_VERSION" .
 
-# Read build arguments into an array
-readarray -t BUILD_ARGS < <(node scripts/read-unicorn-build-args.mjs)
+# Read build arguments into an array using portable syntax
+BUILD_ARGS=()
+while IFS= read -r arg; do
+    BUILD_ARGS+=("$arg")
+done < <(node scripts/read-unicorn-build-args.mjs)
 docker buildx build --push --platform linux/arm64/v8,linux/amd64 "${BUILD_ARGS[@]}" --tag ghcr.io/defenseunicorns/pepr/private/controller:v"$FULL_VERSION" .
 
 npm install 
