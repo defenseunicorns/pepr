a1366e00772d9bb4a0b30cffb4b3704686e7d35e
Case Wylie <cmwylie19@defenseunicorns.com>
2025-04-06 09:20:39 -0400

    chore: update test
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- integration/cli/crd.test.ts
+++ integration/cli/crd.test.ts
@@ -13,8 +13,8 @@ import yaml from "js-yaml";
 
 const FILE = path.basename(__filename);
 const HERE = __dirname;
-
-
+let crd = ""
+let crdJSON: CustomResourceDefinition;
 describe("crd", () => {
   const workdir = new Workdir(`${FILE}`, `${HERE}/../testroot/cli`);
   
@@ -65,114 +65,128 @@ describe("crd", () => {
       it("creates a new CRD at crds/<kind>.yaml", async () => {
         try {
           await fs.access(crdFilePath);
+          crd = await fs.readFile(crdFilePath, "utf8");
+          crdJSON = yaml.load(crd) as CustomResourceDefinition;
           expect(true).toBe(true);
+          expect(crdJSON).toBeDefined();
+          expect(crd).toBeDefined();
         } catch (err) {
           expect(err).toBeFalsy();
         }
       });
     });
 
-      it("CRD is accurate based on TypeScript types file used in generation", async () => {
-        try {
-          const crd = await fs.readFile(crdFilePath, "utf8");
-          const crdJSON = yaml.load(crd) as CustomResourceDefinition;
-
-          // CRD metadata
-          expect(crdJSON.metadata.name).toEqual(`${plural}.${group}.pepr.dev`);
-
-          // CRD Spec - Top Level with names
-          expect(crdJSON.spec.group).toEqual(`${group}.pepr.dev`);
-          expect(crdJSON.spec.names.kind).toEqual(kind);
-          expect(crdJSON.spec.names.plural).toEqual(plural);
-          expect(crdJSON.spec.names.singular).toEqual(kind.toLowerCase());
-          expect(crdJSON.spec.names.shortNames).toEqual([shortName]);
-          expect(crdJSON.spec.scope).toEqual(scope);
-
-          // CRD Spec - Versions
-          expect(crdJSON.spec.versions[0].name).toEqual(version);
-          expect(crdJSON.spec.versions[0].served).toEqual(true);
-          expect(crdJSON.spec.versions[0].storage).toEqual(true);
-
-          // CRD Spec - Version Schema
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.type).toEqual("object");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.type).toEqual("object");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.description).toEqual("MemcachedSpec defines the desired state of Memcached");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties).toBeDefined();
-
-          // Size is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.size).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.size.type).toEqual("array");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.size.items!.type).toEqual("number");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.size.description).toEqual("Size defines the number of Memcache instances");
-
-
-          // ContainerPort is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.containerPort).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.containerPort.type).toEqual("number");
-
-          // Config is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.type).toEqual("object");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.language).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.language.type).toEqual("array");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.language.items!.type).toEqual("string");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.timezone).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.timezone.type).toEqual("number");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.description).toEqual("Application specific configuration");
-
-          // Correct required fields
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.required).toEqual(["containerPort"]);
-
-          // Status is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.type).toEqual("object");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.description).toEqual("MemcachedStatus defines the observed state of Memcached");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties).toBeDefined();
-
-          // Status conditions are correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.type).toEqual("array");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.type).toEqual("object");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.description).toEqual("Condition contains details for one aspect of the current state of this API Resource.");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties).toBeDefined();
-
-          // Status conditinos lastTransitionTime is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.lastTransitionTime).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.lastTransitionTime.type).toEqual("string");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.lastTransitionTime.format).toEqual("date-time");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.lastTransitionTime.description).toContain("lastTransitionTime is the last time the condition transitioned from one status to another. This is not guaranteed to be set in happensBefore order across different conditions for a given object. It may be unset in some circumstances.");
-
-          // Status conditions message is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.message).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.message.type).toEqual("string");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.message.description).toContain("message is a human readable message indicating details about the transition. This may be an empty string.");
-
-          // Status conditions observedGeneration is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.observedGeneration).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.observedGeneration.type).toEqual("number");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.observedGeneration.description).toContain("observedGeneration represents the .metadata.generation that the condition was set based upon.");
-
-          // Status conditions reason is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.reason).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.reason.type).toEqual("string");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.reason.description).toContain("reason contains a programmatic identifier indicating the reason for the condition's last transition.")
-          
-          // Status conditions status is correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.status).toBeDefined();
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.status.type).toEqual("string");
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.status.description).toContain("status of the condition, one of True, False, Unknown.");
-
-          // status required fields are correct
-          expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.required).toStrictEqual(["lastTransitionTime", "message", "reason", "status"]);
-
-        } catch (err) {
-          expect(err).toBeFalsy();
-        }
-      });
+    it("should produce CRD with accurate metadata", () => {
+      expect(crdJSON.metadata.name).toEqual(`${plural}.${group}.pepr.dev`);
     });
+
+    it("should produce CRD with accurate top level spec", ()=> {
+      expect(crdJSON.spec.group).toEqual(`${group}.pepr.dev`);
+      expect(crdJSON.spec.names.kind).toEqual(kind);
+      expect(crdJSON.spec.names.plural).toEqual(plural);
+      expect(crdJSON.spec.names.singular).toEqual(kind.toLowerCase());
+      expect(crdJSON.spec.names.shortNames).toEqual([shortName]);
+      expect(crdJSON.spec.scope).toEqual(scope);
+    })
+
+    it("should produce CRD with accurate spec versions", () => {
+      expect(crdJSON.spec.versions[0].name).toEqual(version);
+      expect(crdJSON.spec.versions[0].served).toEqual(true);
+      expect(crdJSON.spec.versions[0].storage).toEqual(true);
+    })
+
+    it("should produce CRD with accurate spec versions schema", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.type).toEqual("object");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.type).toEqual("object");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.description).toEqual("MemcachedSpec defines the desired state of Memcached");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties).toBeDefined();
+    });
+
+    it("should produce CRD with size property based on TypeScript Type with size", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.size).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.size.type).toEqual("array");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.size.items!.type).toEqual("number");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.size.description).toEqual("Size defines the number of Memcache instances");
+    })
+
+    it("should produce CRD with size property based on TypeScript Type with containerPort", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.containerPort).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.containerPort.type).toEqual("number");
+    })
+
+    it("should produce CRD with containerPort property based on TypeScript Type with config", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.type).toEqual("object");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.language).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.language.type).toEqual("array");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.language.items!.type).toEqual("string");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.timezone).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.properties!.timezone.type).toEqual("number");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.config.description).toEqual("Application specific configuration");
+    });
+
+    it("should container correct required fields for spec", () =>{
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.spec.required).toEqual(["containerPort"]);
+    })
+
+    it("should produce CRD with accurate top level status", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.type).toEqual("object");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.description).toEqual("MemcachedStatus defines the observed state of Memcached");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties).toBeDefined();
+    })
+
+    it("should produce CRD with accurate status conditions", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.type).toEqual("array");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.type).toEqual("object");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.description).toEqual("Condition contains details for one aspect of the current state of this API Resource.");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties).toBeDefined();
+    });
+
+    it("should produce CRD with accurate status conditions lastTransitionTime", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.lastTransitionTime).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.lastTransitionTime.type).toEqual("string");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.lastTransitionTime.format).toEqual("date-time");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.lastTransitionTime.description).toContain("lastTransitionTime is the last time the condition transitioned from one status to another. This is not guaranteed to be set in happensBefore order across different conditions for a given object. It may be unset in some circumstances.");
+    });
+
+    it("should produce CRD with accurate status conditions message", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.message).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.message.type).toEqual("string");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.message.description).toContain("message is a human readable message indicating details about the transition. This may be an empty string.");
+    })
+
+    it("should produce CRD with accurate status conditions observedGeneration", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.observedGeneration).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.observedGeneration.type).toEqual("number");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.observedGeneration.description).toContain("observedGeneration represents the .metadata.generation that the condition was set based upon.");
+    })
+
+    it("should produce CRD with accurate status conditions reason", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.reason).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.reason.type).toEqual("string");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.reason.description).toContain("reason contains a programmatic identifier indicating the reason for the condition's last transition.")
+      
+    })
+
+    it("should produce CRD with accurate status conditions status", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.status).toBeDefined();
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.status.type).toEqual("string");
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.properties.status.description).toContain("status of the condition, one of True, False, Unknown.");
+    })
+
+    it("should produce CRD with accurate required fields for status", () => {
+      expect(crdJSON.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.conditions.items!.required).toStrictEqual(["lastTransitionTime", "message", "reason", "status"]);
+
+    })
+
+  });
 
 });
+        // Not optional, subresource must be present and it is empty
+        // eslint-disable-next-line @typescript-eslint/no-empty-object-type
