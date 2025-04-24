99f44197242cc99e6e04fd7c44057c0ed0a853d0
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-04-24 15:21:15 -0500

    chore: replace process.exit() with graceful error handling (#2062)
    ## Description
    
    This PR removes a `process.exit()` call and replaces it with improved
    error-handling. It also adds test coverage for untested execution paths.
    
    ## Related Issue
    
    Fixes #1799
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [x] Other (security config, docs update, etc)
    
    ## Checklist before merging
    - [x] Unit,
    [Journey](https://github.com/defenseunicorns/pepr/tree/main/journey),
    [E2E Tests](https://github.com/defenseunicorns/pepr-excellent-examples),
    [docs](https://github.com/defenseunicorns/pepr/tree/main/docs),
    [adr](https://github.com/defenseunicorns/pepr/tree/main/adr) added or
    updated as needed
    - [x] [Contributor Guide
    Steps](https://docs.pepr.dev/main/contribute/#submitting-a-pull-request)
    followed

---
+++ src/lib/controller/createHooks.test.ts
@@ -0,0 +1,64 @@
+import { describe, beforeEach, jest, it, expect } from "@jest/globals";
+import { createControllerHooks } from "./createHooks";
+import * as watchProcessor from "../processors/watch-processor";
+import { Capability } from "../core/capability";
+
+const setupWatchMock = jest.spyOn(watchProcessor, "setupWatch");
+
+const capabilities = [
+  new Capability({
+    name: "test-capability",
+    description: "test capability for hooks",
+  }),
+];
+
+const opts = {
+  beforeHook: jest.fn(),
+  afterHook: jest.fn(),
+  deferStart: true,
+};
+
+it("should not call setupWatch when not in watch or dev mode", async () => {
+  createControllerHooks(opts, capabilities, []).onReady!();
+  expect(setupWatchMock).not.toHaveBeenCalled();
+});
+
+describe.each([["watch"], ["dev"]])("when running in '%s' mode", mode => {
+  beforeEach(() => {
+    process.env.PEPR_WATCH_MODE = mode === "watch" ? "true" : undefined;
+    process.env.PEPR_MODE = mode === "dev" ? "dev" : undefined;
+    jest.resetAllMocks();
+  });
+
+  describe("when ignored namespaces are not provided", () => {
+    it("should include all namespaces", async () => {
+      createControllerHooks(opts, capabilities).onReady!();
+      expect(setupWatchMock).toHaveBeenCalledWith(capabilities, []);
+    });
+  });
+  it("should call setupWatch when controller is ready", async () => {
+    setupWatchMock.mockImplementationOnce(() => {
+      return;
+    });
+
+    createControllerHooks(opts, capabilities, ["some-namespace"]).onReady!();
+
+    expect(setupWatchMock).toHaveBeenCalledTimes(1);
+    expect(setupWatchMock).toHaveBeenCalledWith(
+      capabilities,
+      expect.arrayContaining(["some-namespace"]),
+    );
+  });
+
+  it("should throw an error when setupWatch fails", async () => {
+    setupWatchMock.mockImplementationOnce(() => {
+      throw new Error("Test watch setup error");
+    });
+
+    await expect(createControllerHooks(opts, capabilities, []).onReady!()).rejects.toThrow(
+      "WatchError: Could not set up watch.",
+    );
+
+    expect(setupWatchMock).toHaveBeenCalledTimes(1);
+  });
+});
