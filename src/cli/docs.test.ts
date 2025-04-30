ed966f00bac6a4f0fab60614d7f9bff37caff5e4
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-04-30 16:50:03 -0500

    Run each subcommand in all templated sections

--- src/cli/docs.test.ts
+++ src/cli/docs.test.ts
@@ -7,12 +7,13 @@ const execPromise = promisify(childProcess.exec);
 
 describe("Pepr CLI Help Menu", () => {
   const cliPath = path.resolve(process.cwd(), "src/cli.ts");
+  const command = async (subcommand: string = "") =>
+    await execPromise(`npx ts-node ${cliPath} ${subcommand} --help`);
+
   describe("when `pepr --help` executes", () => {
     it("should display the help menu with correct information", async () => {
-      const command = `npx ts-node ${cliPath} --help`;
-
       try {
-        const { stdout, stderr } = await execPromise(command);
+        const { stdout, stderr } = await command();
 
         expect(stdout).toBeTruthy();
         expect(stdout).toContain("-V, --version          output the version number");
@@ -27,68 +28,92 @@ describe("Pepr CLI Help Menu", () => {
   });
 
   describe("when `pepr build --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("build");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
 
   describe("when `pepr crd --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("crd");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
 
   describe("when `pepr crd create --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("crd create");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
 
   describe("when `pepr crd generate --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("crd generate");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
 
   describe("when `pepr deploy --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("deploy");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
 
   describe("when `pepr dev --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("dev");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
   describe("when `pepr format --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("format");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
   describe("when `pepr init --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("init");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
   describe("when `pepr kfc --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("kfc");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
   describe("when `pepr monitor --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("monitor");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
   describe("when `pepr update --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("update");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
   describe("when `pepr uuid --help` executes", () => {
-    it("does something", () => {
-      expect(true).toBeTruthy();
+    it("does something", async () => {
+      const { stdout, stderr } = await command("uuid");
+      expect(stdout).toBeTruthy();
+      expect(stderr).toBeFalsy();
     });
   });
 });
