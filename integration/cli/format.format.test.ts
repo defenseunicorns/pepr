0230d0578ffa88394d6aee6e6cac5c3e4cf0c218
Case Wylie <cmwylie19@defenseunicorns.com>
2025-05-12 16:23:37 -0400

    chore: format integration test
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

---
+++ integration/cli/format.format.test.ts
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023-Present The Pepr Authors
+
+import { beforeAll, describe, expect, it } from "@jest/globals";
+import * as path from "node:path";
+import * as fs from "node:fs/promises";
+import { Workdir } from "../helpers/workdir";
+import * as time from "../helpers/time";
+import * as pepr from "../helpers/pepr";
+import { Result } from "../helpers/cmd";
+
+const FILE = path.basename(__filename);
+const HERE = __dirname;
+
+describe("build", () => {
+  const workdir = new Workdir(`${FILE}`, `${HERE}/../testroot/cli`);
+
+  beforeAll(async () => {
+    await workdir.recreate();
+  }, time.toMs("60s"));
+
+  describe("when building a module", () => {
+    const id = FILE.split(".").at(1);
+    const testModule = `${workdir.path()}/${id}`;
+    let formatOutput: Result;
+
+    beforeAll(async () => {
+      await fs.rm(testModule, { recursive: true, force: true });
+      const initArgs = [
+        `--name ${id}`,
+        `--description ${id}`,
+        `--errorBehavior reject`,
+        `--uuid format`,
+        "--confirm",
+        "--skip-post-init",
+      ].join(" ");
+      await pepr.cli(workdir.path(), { cmd: `pepr init ${initArgs}` });
+      await pepr.tgzifyModule(testModule);
+      await pepr.cli(testModule, { cmd: `npm install` });
+
+      formatOutput = await pepr.cli(testModule, { cmd: `pepr format` });
+    }, time.toMs("3m"));
+
+    it("should execute 'pepr form,at'", () => {
+      expect(formatOutput.exitcode).toBe(0);
+      expect(formatOutput.stderr.join("").trim()).toContain("");
+      expect(formatOutput.stdout.join("").trim()).toContain("✅ Module formatted");
+    });
+  });
+});
