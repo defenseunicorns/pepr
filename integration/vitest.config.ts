d08f1b63e04eaca8c21a8fe972b6eef22c19ec46
Case Wylie <cmwylie19@defenseunicorns.com>
2025-06-16 07:52:08 -0400

    chore: integration directory
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- integration/vitest.config.ts
+++ integration/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["integration/**/*.test.ts"],
-    exclude: ["node_modules", "dist", "coverage"],
+    exclude: ["**/node_modules/**", "dist", "coverage"],
     environment: "node",
   },
 });
