7bb6d18f18f8360a2457495ec000007454f07383
Case Wylie <cmwylie19@defenseunicorns.com>
2025-06-18 15:31:36 -0400

    chore: vitest config for integration
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- config/vitest.integration.config.ts
+++ config/vitest.integration.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
-
+import path from "path";
 export default defineConfig({
   test: {
     include: ["integration/**/*.test.ts"],
     exclude: ["**/node_modules/**", "dist", "coverage"],
     environment: "node",
+    tsconfig: path.resolve(__dirname, "tsconfig.integration.json"),
   },
 });
