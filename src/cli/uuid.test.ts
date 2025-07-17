b42592ea388601b86b4c89dff6ff7570c6f6e372
Case Wylie <cmwylie19@defenseunicorns.com>
2025-07-17 07:32:18 -0400

    Update src/cli/uuid.test.ts
    Co-authored-by: Sam Mayer <sam.mayer@defenseunicorns.com>

--- src/cli/uuid.test.ts
+++ src/cli/uuid.test.ts
@@ -136,4 +136,13 @@ describe("uuid CLI command", () => {
     expect(logSpy).toHaveBeenCalledWith("--------------------------------------------");
     expect(logSpy).toHaveBeenCalledWith("1234\tTest annotation");
   });
+
+  it("should log UUID and description when deployment has a matching UUID", async () => {
+    await program.parseAsync(["uuid", "asdf"], { from: "user" });
+
+    expect(logSpy).toHaveBeenCalledWith("UUID\t\tDescription");
+    expect(logSpy).toHaveBeenCalledWith("--------------------------------------------");
+    expect(logSpy).toHaveBeenCalledWith("asdf\tAnother annotation");
+    expect(logSpy).not.toHaveBeenCalledWith("1234\tTest annotation");
+  });
 });
