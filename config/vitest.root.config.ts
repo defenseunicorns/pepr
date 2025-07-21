36c7891e8fe4632f19d2f6d3bec2db105242de8a
Case Wylie <cmwylie19@defenseunicorns.com>
2025-07-21 12:46:19 -0400

    chore: exclude build-artifacts test in unit tests
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- config/vitest.root.config.ts
+++ config/vitest.root.config.ts
@@ -3,7 +3,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["src/**/*.test.ts", "hack/**/*.test.ts"],
-    exclude: ["node_modules", "dist", "pepr/**", "src/templates/**", "coverage"],
+    exclude: [
+      "node_modules",
+      "dist",
+      "pepr/**",
+      "src/templates/**",
+      "coverage",
+      "src/build-artifact.test.ts",
+    ],
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
