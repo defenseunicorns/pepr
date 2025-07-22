cbbb92817940d9992239af23481b300a1a61a1e8
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-07-22 15:34:18 -0500

    Remove deprecated journey-style tests for format

--- journey/entrypoint.test.ts
+++ journey/entrypoint.test.ts
@@ -7,7 +7,6 @@ import { before } from "./before";
 import { peprBuild } from "./pepr-build";
 import { peprDeploy } from "./pepr-deploy";
 import { peprDev } from "./pepr-dev";
-import { peprFormat } from "./pepr-format";
 import { peprInit } from "./pepr-init";
 import { removeFolder } from "./utils";
 import { outputDir } from "./pepr-build.helpers";
@@ -21,8 +20,6 @@ afterAll(async () => {
   await removeFolder(outputDir);
 });
 describe("Journey: `npx pepr init`", { timeout: 1000 * 5 * 60 }, peprInit);
-
-describe("Journey: `npx pepr format`", { timeout: 1000 * 5 * 60 }, peprFormat);
 
 describe("Journey: `npx pepr build`", { timeout: 1000 * 5 * 60 }, peprBuild);
 
