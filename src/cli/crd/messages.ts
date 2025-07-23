e95f3a5abf88be22dd46945dea2f737e9efcef8d
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-07-23 13:43:06 -0500

    Add SPDX header

--- src/cli/crd/messages.ts
+++ src/cli/crd/messages.ts
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023-Present The Pepr Authors
+
 export const ErrorMessages = {
   MISSING_DETAILS: "Missing 'details' variable declaration.",
   INVALID_SCOPE: (scope: string): string =>
