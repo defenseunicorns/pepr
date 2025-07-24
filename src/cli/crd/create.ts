d92d36000db84f15ec7c152f9649d08a2af477ca
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-07-24 10:32:24 -0500

    Remove default value from required option

--- src/cli/crd/create.ts
+++ src/cli/crd/create.ts
@@ -22,7 +22,7 @@ const create = new Command("create")
   .requiredOption("-g, --group <group>", "API group (e.g. cache)")
   .requiredOption("-k, --kind <kind>", "Kind name (e.g. memcached)")
   .option("-p, --plural <plural>", "Plural name for CRD (e.g. memcacheds)")
-  .requiredOption("-s, --short-name <name>", "Short name for CRD (e.g. mc)", "")
+  .requiredOption("-s, --short-name <name>", "Short name for CRD (e.g. mc)")
   .requiredOption("-v, --version <version>", "API version (e.g. v1alpha1)")
   .action(async ({ group, version, kind, domain, scope, plural, shortName }) => {
     Log.warn("This feature is currently in alpha.");
