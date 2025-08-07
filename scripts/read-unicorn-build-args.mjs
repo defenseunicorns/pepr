7d4c52a0aa55f57399439fbf7faa461a7bc8b50a
Case Wylie <cmwylie19@defenseunicorns.com>
2025-08-07 15:40:58 -0400

    chore: update base for upstream to for an image with zero high severity vulns (#2516)
    ## Description
    
    ```bash
    > grype pepr:dev     
     ✔ Loaded image                                                                                            pepr:dev 
     ✔ Parsed image                             sha256:928c6f028119117f3e839d9161afaaa9fc0d6aee33a3ff59994f8b4cebd315d7 
     ✔ Cataloged contents                              117b0acd3e9b534bf8ebae69327d3f98ef0d5c7f022001f1f21708ea0b542331 
       ├── ✔ Packages                        [473 packages]  
       ├── ✔ Executables                     [46 executables]  
       ├── ✔ File metadata                   [538 locations]  
       └── ✔ File digests                    [538 files]  
     ✔ Scanned for vulnerabilities     [6 vulnerability matches]  
       ├── by severity: 0 critical, 0 high, 0 medium, 6 low, 0 negligible
       └── by status:   0 fixed, 6 not-fixed, 0 ignored 
    NAME           INSTALLED   FIXED-IN  TYPE  VULNERABILITY   SEVERITY  EPSS%  RISK  
    busybox        1.37.0-r18            apk   CVE-2025-46394  Low        2.90  < 0.1  
    busybox-binsh  1.37.0-r18            apk   CVE-2025-46394  Low        2.90  < 0.1  
    ssl_client     1.37.0-r18            apk   CVE-2025-46394  Low        2.90  < 0.1  
    busybox        1.37.0-r18            apk   CVE-2024-58251  Low        2.60  < 0.1  
    busybox-binsh  1.37.0-r18            apk   CVE-2024-58251  Low        2.60  < 0.1  
    ssl_client     1.37.0-r18            apk   CVE-2024-58251  Low        2.60  < 0.1
    ```
    
    ## Related Issue
    
    Fixes #2184
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Other (security config, docs update, etc)
    
    ## Checklist before merging
    - [ ] Unit,
    [Journey](https://github.com/defenseunicorns/pepr/tree/main/journey),
    [E2E Tests](https://github.com/defenseunicorns/pepr-excellent-examples),
    [docs](https://github.com/defenseunicorns/pepr/tree/main/docs),
    [adr](https://github.com/defenseunicorns/pepr/tree/main/adr) added or
    updated as needed
    - [ ] [Contributor Guide
    Steps](https://docs.pepr.dev/main/contribute/#submitting-a-pull-request)
    followed
    
    ---------
    
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- scripts/read-unicorn-build-args.mjs
+++ scripts/read-unicorn-build-args.mjs
@@ -3,7 +3,6 @@
 
 import { readFileSync } from "fs";
 
-
 try {
   const dockerfileContent = readFileSync("./config/Dockerfile", "utf8");
   const fromStatements = dockerfileContent.match(/^FROM\s+([^\s]+)\s+/gm);
@@ -13,13 +12,15 @@ try {
     process.exit(1);
   }
 
-  const buildImage = fromStatements[0].split(" ")[1].trim(); 
-  const baseImage = fromStatements[1].split(" ")[1].trim();  
+  const buildImage = fromStatements[0].split(" ")[1].trim();
+  const baseImage = fromStatements[1].split(" ")[1].trim();
 
   console.log("--build-arg");
   console.log(`BUILD_IMAGE=${buildImage}`);
   console.log("--build-arg");
   console.log(`BASE_IMAGE=${baseImage}`);
+  console.log("--build-arg");
+  console.log(`REQUIRE_CHOWN=false`);
 } catch (error) {
   console.error("Error reading or parsing the Dockerfile:", error);
   process.exit(1);
