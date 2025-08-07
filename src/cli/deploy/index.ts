6c5dc2464671416854d875937cc84082a94c06cc
Case Wylie <cmwylie19@defenseunicorns.com>
2025-08-07 12:48:34 -0400

    chore: deploy test (#2508)
    ## Description
    
    unit test for deploy
    
    ## Related Issue
    
    Fixes #2396 
    <!-- or -->
    Relates to #
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [x] Other (security config, docs update, etc)
    
    ## Checklist before merging
    - [x] Unit,
    [Journey](https://github.com/defenseunicorns/pepr/tree/main/journey),
    [E2E Tests](https://github.com/defenseunicorns/pepr-excellent-examples),
    [docs](https://github.com/defenseunicorns/pepr/tree/main/docs),
    [adr](https://github.com/defenseunicorns/pepr/tree/main/adr) added or
    updated as needed
    - [x] [Contributor Guide
    Steps](https://docs.pepr.dev/main/contribute/#submitting-a-pull-request)
    followed
    
    ---------
    
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>
    Co-authored-by: Sam Mayer <sam.mayer@defenseunicorns.com>

---
+++ src/cli/deploy/index.ts
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023-Present The Pepr Authors
+
+import { Command } from "commander";
+import { deployImagePullSecret } from "../../lib/assets/deploy";
+import { validateImagePullSecretDetails, generateImagePullSecret } from "./imagePullSecret";
+import { getUserConfirmation } from "./userConfirmation";
+import { buildAndDeployModule } from "./buildAndDeploy";
+
+export default function (program: Command): void {
+  program
+    .command("deploy")
+    .description("Deploy a Pepr Module")
+    .option("-E, --docker-email <email>", "Email for Docker registry.")
+    .option("-P, --docker-password <password>", "Password for Docker registry.")
+    .option("-S, --docker-server <server>", "Docker server address.")
+    .option("-U, --docker-username <username>", "Docker registry username.")
+    .option("-f, --force", "Force deploy the module, override manager field.")
+    .option("-i, --image <image>", "Override the image tag.")
+    .option("-p, --pull-secret <name>", "Deploy imagePullSecret for Controller private registry.")
+    .option("-y, --yes", "Skip confirmation prompts.")
+    .action(async opts => {
+      const valResp = validateImagePullSecretDetails(opts);
+      if (!valResp.valid) {
+        console.error(valResp.error);
+        process.exit(1);
+      }
+
+      if (opts.pullSecret) {
+        await deployImagePullSecret(generateImagePullSecret(opts), opts.pullSecret);
+        return;
+      }
+
+      if (!(await getUserConfirmation(opts))) {
+        process.exit(0);
+      }
+
+      await buildAndDeployModule(opts.image, opts.force);
+    });
+}
---
+++ src/cli/deploy/userConfirmation.test.ts
@@ -0,0 +1,32 @@
+import { describe, afterEach, vi, it, expect } from "vitest";
+import { getUserConfirmation } from "./userConfirmation";
+import prompt from "prompts";
+
+vi.mock("prompts", () => ({
+  default: vi.fn(),
+}));
+
+describe("getUserConfirmation", () => {
+  const mockPrompt = vi.mocked(prompt);
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("returns true if yes option is passed", async () => {
+    const result = await getUserConfirmation({ yes: true });
+    expect(result).toBe(true);
+  });
+
+  it("returns true if user confirms", async () => {
+    mockPrompt.mockResolvedValue({ yes: true });
+    const result = await getUserConfirmation({ yes: false });
+    expect(result).toBe(true);
+  });
+
+  it("returns false if user declines", async () => {
+    mockPrompt.mockResolvedValue({ yes: false });
+    const result = await getUserConfirmation({ yes: false });
+    expect(result).toBe(false);
+  });
+});
