55e5ddb1df29f73306ff18616653428d183984a7
Amber <amber.j.fryar@gmail.com>
2026-05-22 14:01:02 -0600

    chore: greptile change

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -23,6 +23,6 @@ module.exports = {
   ignores: [
     // prevent header-max-length error on long, dependabot-gen'd commits titles
     //  https://github.com/dependabot/dependabot-core/issues/2445
-    (message) => /^chore: bump .+ from .+ to .+$/m.test(message)
+    (message) => /^(chore|deps)(\(.+\))?: bump .+ from .+ to .+$/m.test(message)
   ],
 };
