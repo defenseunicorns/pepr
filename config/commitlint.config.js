83a76e3b1228182b35f5ba95c9528090af57affb
Amber <amber.j.fryar@gmail.com>
2026-05-22 13:55:53 -0600

    chore: add deps as a prefix

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -1,5 +1,25 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "deps",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
   ignores: [
     // prevent header-max-length error on long, dependabot-gen'd commits titles
     //  https://github.com/dependabot/dependabot-core/issues/2445
